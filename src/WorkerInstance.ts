94696e45e8d7ca5a02be9481073417ed7ac942bf
Prithviraj Kadiyala <pkadiyala@kipr.org>
2020-04-27 02:48:50 -0500

    Motor_Positiona and Servo_Position Formulas

--- src/WorkerInstance.ts
+++ src/WorkerInstance.ts
@@ -24,6 +24,15 @@ class WorkerInstance {
     nextState.motor1_speed = this.registers_[64]*256+this.registers_[65];
     nextState.motor2_speed = this.registers_[66]*256+this.registers_[67];
     nextState.motor3_speed = this.registers_[68]*256+this.registers_[69];
+    nextState.motor0_position = this.registers_[137]*16777216+this.registers_[138]*65536+this.registers_[139]*256+this.registers_[140];
+    nextState.motor1_position = this.registers_[141]*16777216+this.registers_[142]*65536+this.registers_[143]*256+this.registers_[144];
+    nextState.motor2_position = this.registers_[145]*16777216+this.registers_[146]*65536+this.registers_[147]*256+this.registers_[148];
+    nextState.motor3_position = this.registers_[149]*16777216+this.registers_[150]*65536+this.registers_[151]*256+this.registers_[152];
+    nextState.servo0_position = this.registers_[78]*256+this.registers_[79];
+    nextState.servo1_position = this.registers_[80]*256+this.registers_[81];
+    nextState.servo2_position = this.registers_[82]*256+this.registers_[83];
+    nextState.servo3_position = this.registers_[84]*256+this.registers_[85];
+    
     const total_speed = (nextState.motor0_speed + nextState.motor3_speed)/1500;
     const new_time = Date.now()/1000
     const time_change = new_time - this.time_;
