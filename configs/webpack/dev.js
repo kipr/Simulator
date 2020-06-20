05d75545f250e00ed1395f41044cdee2e8ec44e2
Nafis Zaman <nazam@microsoft.com>
2020-06-19 18:02:16 -0700

    Use dev config on yarn watch

--- configs/webpack/dev.js
+++ configs/webpack/dev.js
@@ -1,27 +1,20 @@
 // development config
 const merge = require('webpack-merge');
-const webpack = require('webpack');
+const { join } = require('path');
+
 const commonConfig = require('./common');
+const { cwd } = require('process');
 
 module.exports = merge(commonConfig, {
   mode: 'development',
-  entry: [
-    'react-hot-loader/patch', // activate HMR for React
-    'webpack-dev-server/client?http://localhost:8080',// bundle the client for webpack-dev-server and connect to the provided endpoint
-    'webpack/hot/only-dev-server', // bundle the client for hot reloading, only- means to only hot reload for successful updates
-    './index.tsx', // the entry point of our app
-    './worker.ts'
-  ],
-  devServer: {
-    hot: true, // enable HMR on the server
-    historyApiFallback: true,
+  entry: {
+    app: './index.tsx',
+    worker: './worker.ts'
   },
   output: {
-    filename: '[name].js'
+    filename: 'js/[name].min.js',
+    path: join(cwd(), 'dist'),
   },
   devtool: 'cheap-module-eval-source-map',
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(), // enable HMR globally
-    new webpack.NamedModulesPlugin(), // prints more readable module names in the browser console on HMR updates
-  ],
+  plugins: [],
 });
