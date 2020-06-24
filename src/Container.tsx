83d8c99a7c02c5d5ce700ab1f9d4b424821db4f2
tcorbly <tcorbly@kipr.org>
2020-06-23 21:57:08 -0500

    Wheels spin with phys obj. And parts of sim public

--- src/Container.tsx
+++ src/Container.tsx
@@ -12,21 +12,7 @@ interface ContainerState {
 type Props = ContainerProps;
 type State = ContainerState;
 
-document.body.onload = () => {
-		const canvas = document.getElementById('simview') as HTMLCanvasElement;
-		const holder = document.getElementById('right') as HTMLDivElement;
-		const engine = new Babylon.Engine(canvas, true, {preserveDrawingBuffer: true, stencil: true});
-		const space = new Sim.Space();
-		const scene = space.createScene(engine, canvas);
-		engine.runRenderLoop(function(){
-			scene.render();
-		});
-		
-		canvas.addEventListener('resize', function(){
-			engine.resize();
-			console.log('Yay!');
-		})
-};
+
 
 
 export class Container extends React.Component<Props, State> {
@@ -57,6 +43,33 @@ export class Container extends React.Component<Props, State> {
 			props, state
 		} = this
 
+		document.body.onload = () => {
+			const canvas = document.getElementById('simview') as HTMLCanvasElement;
+			const holder = document.getElementById('right') as HTMLDivElement;
+			const engine = new Babylon.Engine(canvas, true, {preserveDrawingBuffer: true, stencil: true});
+			const space = new Sim.Space(engine, canvas);
+			space.createScene();
+			engine.runRenderLoop(function(){
+				space.scene.executeWhenReady(function () {
+					// space.scene.getMeshByID('pw-mt11040').rotationQuaternion = undefined;
+					space.scene.getMeshByID('pw-mt11040').rotationQuaternion = space.wheel1.rotationQuaternion;
+					space.scene.getMeshByID('black high gloss plastic').rotationQuaternion = space.wheel1.rotationQuaternion;
+					space.scene.getMeshByID('matte rubber').rotationQuaternion = space.wheel1.rotationQuaternion;
+
+					space.scene.getMeshByID('pw-mt11040.2').rotationQuaternion = space.wheel2.rotationQuaternion;
+					space.scene.getMeshByID('black high gloss plastic.2').rotationQuaternion = space.wheel2.rotationQuaternion;
+					space.scene.getMeshByID('matte rubber.2').rotationQuaternion = space.wheel2.rotationQuaternion;
+				});
+				space.scene.render();
+				//space.wheel1_joint.setMotor(state.robot.motor0_speed/500,state.robot.motor0_speed/500);
+				//space.wheel2_joint.setMotor(state.robot.motor3_speed/500,state.robot.motor3_speed/500);
+			});
+			
+			canvas.addEventListener('resize', function(){
+				engine.resize();
+				console.log('Yay!');
+			})
+		};
 		//console.log('qwe')
 
 		return (
