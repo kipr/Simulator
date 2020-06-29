a24366df50024baa22e043a38656521d7fda10fc
tcorbly <tcorbly@kipr.org>
2020-06-29 14:04:08 -0500

    Revert Wheels spin with phys obj parts of sim pub

--- src/Container.tsx
+++ src/Container.tsx
@@ -11,23 +11,42 @@ interface ContainerState {
 }
 type Props = ContainerProps;
 type State = ContainerState;
-
-document.body.onload = () => {
-		const canvas = document.getElementById('simview') as HTMLCanvasElement;
-		const holder = document.getElementById('right') as HTMLDivElement;
-		const engine = new Babylon.Engine(canvas, true, {preserveDrawingBuffer: true, stencil: true});
-		const space = new Sim.Space();
-		const scene = space.createScene(engine, canvas);
+/*
+// body.onload = () => {
+	const motorSpeed1 = WorkerInstance.state.motor0_speed/500;
+	const motorSpeed2 = WorkerInstance.state.motor3_speed/500;
+	// console.log(motorSpeed1+","+motorSpeed2);
+	const canvas = document.getElementById('simview') as HTMLCanvasElement;
+	const holder = document.getElementById('right') as HTMLDivElement;
+	const engine = new Babylon.Engine(canvas, true, {preserveDrawingBuffer: true, stencil: true});
+	const space = new Sim.Space(engine, canvas);
+	space.createScene();
+	space.scene.executeWhenReady(function () {
 		engine.runRenderLoop(function(){
-			scene.render();
-		});
-		
-		canvas.addEventListener('resize', function(){
-			engine.resize();
-			console.log('Yay!');
-		})
-};
+			console.log(motorSpeed1+","+motorSpeed2);
+			space.wheel1_joint.setMotor(motorSpeed1,motorSpeed1);
+			space.wheel2_joint.setMotor(motorSpeed2,motorSpeed2);
+			// space.scene.getMeshByID('pw-mt11040').rotationQuaternion = undefined;
+			space.scene.getMeshByID('pw-mt11040').rotationQuaternion = space.wheel1.rotationQuaternion;
+			space.scene.getMeshByID('black high gloss plastic').rotationQuaternion = space.wheel1.rotationQuaternion;
+			space.scene.getMeshByID('matte rubber').rotationQuaternion = space.wheel1.rotationQuaternion;
 
+			space.scene.getMeshByID('pw-mt11040.2').rotationQuaternion = space.wheel2.rotationQuaternion;
+			space.scene.getMeshByID('black high gloss plastic.2').rotationQuaternion = space.wheel2.rotationQuaternion;
+			space.scene.getMeshByID('matte rubber.2').rotationQuaternion = space.wheel2.rotationQuaternion;
+			
+			
+			space.scene.render();
+			
+		});
+	});
+	
+	canvas.addEventListener('resize', function(){
+		engine.resize();
+		console.log('Yay!');
+	});
+	// }
+*/
 
 export class Container extends React.Component<Props, State> {
 	constructor(props: Props, context?) {
@@ -56,9 +75,43 @@ export class Container extends React.Component<Props, State> {
 		const {
 			props, state
 		} = this
+		/*
+		// body.onload = () => {
+		const motorSpeed1 = state.robot.motor0_speed/500;
+		const motorSpeed2 = state.robot.motor3_speed/500;
+		// console.log(motorSpeed1+","+motorSpeed2);
+		const canvas = document.getElementById('simview') as HTMLCanvasElement;
+		const holder = document.getElementById('right') as HTMLDivElement;
+		const engine = new Babylon.Engine(canvas, true, {preserveDrawingBuffer: true, stencil: true});
+		const space = new Sim.Space(engine, canvas);
+		space.createScene();
+		space.scene.executeWhenReady(function () {
+			engine.runRenderLoop(function(){
+				console.log(motorSpeed1+","+motorSpeed2);
+				space.wheel1_joint.setMotor(motorSpeed1,motorSpeed1);
+				space.wheel2_joint.setMotor(motorSpeed2,motorSpeed2);
+				// space.scene.getMeshByID('pw-mt11040').rotationQuaternion = undefined;
+				space.scene.getMeshByID('pw-mt11040').rotationQuaternion = space.wheel1.rotationQuaternion;
+				space.scene.getMeshByID('black high gloss plastic').rotationQuaternion = space.wheel1.rotationQuaternion;
+				space.scene.getMeshByID('matte rubber').rotationQuaternion = space.wheel1.rotationQuaternion;
 
+				space.scene.getMeshByID('pw-mt11040.2').rotationQuaternion = space.wheel2.rotationQuaternion;
+				space.scene.getMeshByID('black high gloss plastic.2').rotationQuaternion = space.wheel2.rotationQuaternion;
+				space.scene.getMeshByID('matte rubber.2').rotationQuaternion = space.wheel2.rotationQuaternion;
+				
+				
+				space.scene.render();
+				
+			});
+		});
+		
+		canvas.addEventListener('resize', function(){
+			engine.resize();
+			console.log('Yay!');
+		});
+		// }
 		//console.log('qwe')
-
+		*/
 		return (
 			<section id="app">
 				<App robot={state.robot} onRobotChange={this.onRobotChange_} />
--- src/Sim.tsx
+++ src/Sim.tsx
@@ -4,6 +4,18 @@ import 'babylonjs-loaders';
 import Ammo = require('./ammo');
 
 export class Space {
+	public engine: Babylon.Engine;
+	public canvas: HTMLCanvasElement;
+	public scene: Babylon.Scene;
+	public wheel1: Babylon.Mesh;
+	public wheel2: Babylon.Mesh;
+
+	constructor(engine: Babylon.Engine, canvas: HTMLCanvasElement) {
+		this.engine = engine;
+		this.canvas = canvas;
+		this.scene = new Babylon.Scene(engine);
+	}
+
 	public wheel1_joint = new Babylon.MotorEnabledJoint(Babylon.PhysicsJoint.HingeJoint,{
 		mainPivot: new Babylon.Vector3(7.9,-0.2,5.1),//Point relative to the center of the base object
 		connectedPivot: new Babylon.Vector3(0,0,0),//Point relative to the center of the rotating object
@@ -17,76 +29,77 @@ export class Space {
 		mainAxis: new Babylon.Vector3(1,0,0),
 		connectedAxis: new Babylon.Vector3(0,1,0)
 	});
-	public createScene(engine: Babylon.Engine, canvas: HTMLCanvasElement): Babylon.Scene {
-		const scene = new Babylon.Scene(engine);
-
-		const camera = new Babylon.ArcRotateCamera("botcam",10,10,10, new Babylon.Vector3(50,50,50), scene);
+	
+	public createScene() {
+		const camera = new Babylon.ArcRotateCamera("botcam",10,10,10, new Babylon.Vector3(50,50,50), this.scene);
 		camera.setTarget(Babylon.Vector3.Zero());
-		camera.attachControl(canvas, true);
+		camera.attachControl(this.canvas, true);
 
-		const light = new Babylon.HemisphericLight("botlight", new Babylon.Vector3(0,1,0), scene);
+		const light = new Babylon.HemisphericLight("botlight", new Babylon.Vector3(0,1,0), this.scene);
 		light.intensity = 0.7;
 
 		// scene.enablePhysics(new Babylon.Vector3(0,-10,0), new Babylon.OimoJSPlugin(5, Oimo));
-		scene.enablePhysics(new Babylon.Vector3(0,-9.8,0), new Babylon.AmmoJSPlugin(true, Ammo));
+		this.scene.enablePhysics(new Babylon.Vector3(0,-19.6,0), new Babylon.AmmoJSPlugin(true, Ammo));
 
 
-		const ground = Babylon.MeshBuilder.CreateGround("mat", {width:118, height:59, subdivisions:2}, scene);
+		const ground = Babylon.MeshBuilder.CreateGround("mat", {width:118, height:59, subdivisions:2}, this.scene);
 		ground.rotate(new Babylon.Vector3(0,1,0),Math.PI/2);
-		const material = new Babylon.StandardMaterial("mat-material", scene);
-		material.ambientTexture = new Babylon.Texture('static/Surface-A.png',scene);
+		const material = new Babylon.StandardMaterial("mat-material", this.scene);
+		material.ambientTexture = new Babylon.Texture('static/Surface-A.png',this.scene);
 		ground.material = material;
-		ground.physicsImpostor = new Babylon.PhysicsImpostor(ground, Babylon.PhysicsImpostor.BoxImpostor,{mass:0, friction: 2}, scene);
+		ground.physicsImpostor = new Babylon.PhysicsImpostor(ground, Babylon.PhysicsImpostor.BoxImpostor,{mass:0, friction: 2}, this.scene);
 
 		//Colliders
 		const collidersVisible = false;
 
-		const botbody = Babylon.MeshBuilder.CreateBox("botbody", {width:12.3, depth:24.6, height:3}, scene);
+		const botbody = Babylon.MeshBuilder.CreateBox("botbody", {width:12.3, depth:24.6, height:3}, this.scene);
 		botbody.position.y = 4.2;
 		botbody.position.z = -12.3;
 		botbody.isVisible = collidersVisible;
 
-		const loader = Babylon.SceneLoader.ImportMesh("",'static/', 'Simulator_Demobot.glb', scene, function (meshes, particlesystems, skeletons) {
+		const loader = Babylon.SceneLoader.ImportMesh("",'static/', 'Simulator_Demobot.glb', this.scene, function (meshes, particlesystems, skeletons) {
 			const demobot = meshes[0];
-			
+			//console.log(meshes[0]);
 			//Adding meshes to physics root
 			meshes.forEach(element => {
+				// if(element.id == 'Servo Wheel-1'){
+				// 	console.log('Found wheel 1');
+				// }
 				botbody.addChild(element);
 			});
 		});
 
-		const caster = Babylon.MeshBuilder.CreateSphere("caster", {segments:16, diameter:2.2}, scene);
+		const caster = Babylon.MeshBuilder.CreateSphere("caster", {segments:16, diameter:2.2}, this.scene);
 		caster.parent = botbody;
 		caster.position.y = -2.4;
 		caster.position.z = -9;
 		caster.isVisible = collidersVisible;
 
-		const wheel1 = Babylon.MeshBuilder.CreateCylinder("wheel1",{height:0.7, diameter:6.8}, scene);
-		wheel1.position.x = 7.9;
-		wheel1.position.y = 4;
-		wheel1.position.z = -7.2;
-		wheel1.rotation.z = Math.PI/2;
-		wheel1.isVisible = collidersVisible;
+		this.wheel1 = Babylon.MeshBuilder.CreateCylinder("wheel1",{height:0.7, diameter:6.8}, this.scene);
+		this.wheel1.position.x = 7.9;
+		this.wheel1.position.y = 4;
+		this.wheel1.position.z = -7.2;
+		this.wheel1.rotation.z = Math.PI/2;
+		this.wheel1.isVisible = true;//collidersVisible;
 
-		const wheel2 = Babylon.MeshBuilder.CreateCylinder("wheel1",{height:0.7, diameter:6.8}, scene);
-		wheel2.position.x = -7.9;
-		wheel2.position.y = 4;
-		wheel2.position.z = -7.2;
-		wheel2.rotation.z = -Math.PI/2;
-		wheel2.isVisible = collidersVisible;
+		this.wheel2 = Babylon.MeshBuilder.CreateCylinder("wheel1",{height:0.7, diameter:6.8}, this.scene);
+		this.wheel2.position.x = -7.9;
+		this.wheel2.position.y = 4;
+		this.wheel2.position.z = -7.2;
+		this.wheel2.rotation.z = -Math.PI/2;
+		this.wheel2.isVisible = true;//collidersVisible;
 
-		caster.physicsImpostor = new Babylon.PhysicsImpostor(caster, Babylon.PhysicsImpostor.SphereImpostor, {mass: 2, friction: 0.1, restitution:1}, scene);
-		wheel1.physicsImpostor = new Babylon.PhysicsImpostor(wheel1, Babylon.PhysicsImpostor.CylinderImpostor, {mass: 2, friction: 4, restitution:1}, scene);
-		wheel2.physicsImpostor = new Babylon.PhysicsImpostor(wheel2, Babylon.PhysicsImpostor.CylinderImpostor, {mass: 2, friction: 4, restitution:1}, scene);
-		botbody.physicsImpostor = new Babylon.PhysicsImpostor(botbody, Babylon.PhysicsImpostor.BoxImpostor, {mass: 50, friction: 0, restitution:1}, scene);
+		caster.physicsImpostor = new Babylon.PhysicsImpostor(caster, Babylon.PhysicsImpostor.SphereImpostor, {mass: 2, friction: 0.1}, this.scene);
+		this.wheel1.physicsImpostor = new Babylon.PhysicsImpostor(this.wheel1, Babylon.PhysicsImpostor.CylinderImpostor, {mass: 2, friction: 4}, this.scene);
+		this.wheel2.physicsImpostor = new Babylon.PhysicsImpostor(this.wheel2, Babylon.PhysicsImpostor.CylinderImpostor, {mass: 2, friction: 4}, this.scene);
+		botbody.physicsImpostor = new Babylon.PhysicsImpostor(botbody, Babylon.PhysicsImpostor.BoxImpostor, {mass: 50, friction: 0}, this.scene);
 
-		botbody.physicsImpostor.addJoint(wheel1.physicsImpostor,this.wheel1_joint);
+		botbody.physicsImpostor.addJoint(this.wheel1.physicsImpostor,this.wheel1_joint);
 
-		botbody.physicsImpostor.addJoint(wheel2.physicsImpostor,this.wheel2_joint);
+		botbody.physicsImpostor.addJoint(this.wheel2.physicsImpostor,this.wheel2_joint);
 
-		this.wheel1_joint.setMotor(1,-1.75);
-		this.wheel2_joint.setMotor(1,-1.75);
-		return scene;
+		this.wheel1_joint.setMotor(1);
+		this.wheel2_joint.setMotor(1);
 	}
 }
 	
