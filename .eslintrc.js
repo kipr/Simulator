7cc8601a4618806f90f4517fda13e2a63a61c538
Nafis Zaman <nazam@microsoft.com>
2021-03-19 10:48:42 -0700

    Add ESLint rule "no-duplicate-imports"

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,7 @@ module.exports = {
         'arrow-spacing': 'error',
         'no-confusing-arrow': 'error',
         'no-useless-constructor': 'error',
+        'no-duplicate-imports': 'error',
     },
     overrides: [
         {
