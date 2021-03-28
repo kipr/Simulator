a1457442d053552c4197064dc7c3dd8c58721587
Nafis Zaman <nazam@microsoft.com>
2021-03-28 08:15:57 -0700

    Fix warnings about deprecated morgan usage

--- express.js
+++ express.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
 });
 
 app.use(bodyParser.json());
-app.use(morgan());
+app.use(morgan('combined'));
 
 app.post('/compile', (req, res) => {
   // const body = JSON.parse();
