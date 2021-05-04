04d6c40c23242cdaed31b1a7b5787bfac4dc2cd7
Nafis Zaman <nazam@microsoft.com>
2021-05-04 10:59:45 -0700

    Implement touch sensor using mesh intersection

--- src/sensors/TouchSensor.ts
+++ src/sensors/TouchSensor.ts
@@ -5,9 +5,20 @@ import SensorObject from './SensorObject';
 export class TouchSensor implements SensorObject {
   private config_: SensorObject.Config<Sensor.Touch>;
 
-  private visualMesh: Babylon.LinesMesh;
+  // Keeps track of the meshes in the scene that are eligible for intersection checking
+  private eligibleMeshes_: Babylon.AbstractMesh[];
 
-  private readonly VISUAL_MESH_NAME = "etlinemesh";
+  private isIntersecting_ = false;
+  private prevIntersectingMesh_: Babylon.AbstractMesh = null;
+
+  private meshAddedObserver_: Babylon.Observer<Babylon.AbstractMesh>;
+  private meshRemovedObserver_: Babylon.Observer<Babylon.AbstractMesh>;
+
+  // Determines whether the given mesh is eligible for intersection checking
+  // Currently only cans are eligible, but this should be made more flexible
+  private static isMeshEligible = (mesh: Babylon.AbstractMesh) => {
+    return mesh.name.startsWith('Can');
+  };
 
   get sensor(): Sensor.Touch {
     return this.config_.sensor;
@@ -16,24 +27,54 @@ export class TouchSensor implements SensorObject {
   constructor(config: SensorObject.Config<Sensor.Touch>) {
     this.config_ = config;
 
-    this.visualMesh = Babylon.MeshBuilder.CreateLines(
-      this.VISUAL_MESH_NAME,
-      {
-        points: [Babylon.Vector3.Zero(), Babylon.Vector3.Zero()],
-        updatable: true,
-      },
-      this.config_.scene
-    );
+    // Initialize eligible meshes by looking through all meshes in the scene
+    // For performance, might want to find a better way to initialize this
+    this.eligibleMeshes_ = this.config_.scene.meshes.filter(TouchSensor.isMeshEligible);
 
-    this.isVisible = true;
+    // Create observers to update eligible meshes when meshes are added/removed from the scene
+    this.meshAddedObserver_ = this.config_.scene.onNewMeshAddedObservable.add(addedMesh => {
+      if (TouchSensor.isMeshEligible(addedMesh)) {
+        this.eligibleMeshes_.push(addedMesh);
+      }
+    });
+    this.meshRemovedObserver_ = this.config_.scene.onMeshRemovedObservable.add(removedMesh => {
+      const index = this.eligibleMeshes_.findIndex(mesh => mesh.name === removedMesh.name);
+      if (index >= 0) {
+        this.eligibleMeshes_.splice(index, 1);
+      }
+      if (this.prevIntersectingMesh_ && this.prevIntersectingMesh_.name === removedMesh.name) {
+        this.prevIntersectingMesh_ = null;
+      }
+    });
   }
 
   public update(): boolean {
-    return false;
+    // After an intersection, it's likely that the sensor will continue intersecting with the same mesh for a while
+    // So check the previous intersecting mesh upfront
+    if (this.prevIntersectingMesh_ && this.config_.mesh.intersectsMesh(this.prevIntersectingMesh_)) {
+      this.isIntersecting_ = true;
+      return true;
+    }
+
+    for (const eligibleMesh of this.eligibleMeshes_) {
+      // Skip the previous intersecting mesh since we already checked it
+      if (this.prevIntersectingMesh_ && eligibleMesh.id === this.prevIntersectingMesh_.id) continue;
+
+      if (this.config_.mesh.intersectsMesh(eligibleMesh)) {
+        this.isIntersecting_ = true;
+        this.prevIntersectingMesh_ = eligibleMesh;
+        return true;
+      }
+    }
+
+    this.isIntersecting_ = false;
+    this.prevIntersectingMesh_ = null;
+
+    return true;
   }
 
   public getValue(): SensorObject.Value {
-    return SensorObject.Value.FALSE;
+    return this.isIntersecting_ ? SensorObject.Value.TRUE : SensorObject.Value.FALSE;
   }
 
   public updateVisual(): boolean {
@@ -43,15 +84,16 @@ export class TouchSensor implements SensorObject {
   }
 
   public dispose(): void {
-    this.visualMesh.dispose();
+    this.config_.scene.onNewMeshAddedObservable.remove(this.meshAddedObserver_);
+    this.config_.scene.onMeshRemovedObservable.remove(this.meshRemovedObserver_);
   }
 
   public get isVisible(): boolean {
-    return this.visualMesh.isEnabled();
+    return false;
   }
   
   public set isVisible(v: boolean) {
-    this.visualMesh.setEnabled(v);
+    // Touch sensor visibility not yet supported
   }
 }
 
