06751b63360834587f8b2a5c856e93ee30521d87
Nafis Zaman <nazam@microsoft.com>
2021-05-07 14:28:34 -0700

    Improve type safety on sensor instantiate

--- src/sensors/index.ts
+++ src/sensors/index.ts
@@ -5,25 +5,20 @@ import Sensor from './Sensor';
 import SensorObject from './SensorObject';
 import { TouchSensor } from './TouchSensor';
 
-import Dict from '../Dict';
-
 export { default as Sensor } from './Sensor';
 export { default as SensorObject } from './SensorObject';
 export * from './EtSensor';
 export * from './TouchSensor';
 export { default as MAPPINGS } from './mappings';
 
-const SENSOR_OBJECT_CONSTRUCTORS: Dict<SensorObject.Constructor<Sensor>> = {
-  [Sensor.Type.Et]: EtSensor,
-  [Sensor.Type.Touch]: TouchSensor
-};
-
-export const instantiate = <T extends Sensor>(scene: Babylon.Scene, id: string, sensor: T): SensorObject => {
+export const instantiate = (scene: Babylon.Scene, id: string, sensor: Sensor): SensorObject => {
   const mesh = scene.getMeshByID(id) || scene.getMeshByName(id);
   if (!mesh) throw new Error(`Failed to lookup mesh by ID or name (${id})`);
-  
-  const sensorObjectConstructor = SENSOR_OBJECT_CONSTRUCTORS[sensor.type];
-  if (!sensorObjectConstructor) throw new Error(`No associated SensorObject constructor for Sensor type "${Sensor.Type.toString(sensor.type)}"`);
 
-  return new sensorObjectConstructor({ scene, mesh, sensor });
+  switch (sensor.type) {
+    case Sensor.Type.Et:
+      return new EtSensor({ scene, mesh, sensor });
+    case Sensor.Type.Touch:
+      return new TouchSensor({ scene, mesh, sensor });
+  }
 };
