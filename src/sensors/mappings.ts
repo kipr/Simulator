581f94775685a53da89848c3ddd686488a464905
Nafis Zaman <nazam@microsoft.com>
2021-07-28 08:20:25 -0700

    Increase sensor update frequency

--- src/sensors/mappings.ts
+++ src/sensors/mappings.ts
@@ -11,24 +11,24 @@ export default {
     forward: new Babylon.Vector3(0.0, 0.02, 0.0),
     origin: new Babylon.Vector3(0.02, 0.02, -0.015),
     output: Sensor.Output.analog(0),
-    maxUpdateFrequency: 15,
+    maxUpdateFrequency: 3,
   },
   // Front touch sensor
   'collider_touch_front': {
     type: Sensor.Type.Touch,
     output: Sensor.Output.digital(0),
-    maxUpdateFrequency: 15,
+    maxUpdateFrequency: 3,
   },
   // Back left touch sensor
   'collider_touch_back_left': {
     type: Sensor.Type.Touch,
     output: Sensor.Output.digital(1),
-    maxUpdateFrequency: 15,
+    maxUpdateFrequency: 3,
   },
   // Back right touch sensor
   'collider_touch_back_right': {
     type: Sensor.Type.Touch,
     output: Sensor.Output.digital(2),
-    maxUpdateFrequency: 15,
+    maxUpdateFrequency: 3,
   },
 } as Dict<Sensor>;
