6482d8a4a2875493d2219ddb6dd2bba5e84b8561
whawkins-git <will@DESKTOP-C2SV8JO.localdomain>
2022-01-22 23:18:31 -0600

    Remove unnecessary scrollbar, fix video scaling, improve button packing.

--- src/pages/Tutorials.tsx
+++ src/pages/Tutorials.tsx
@@ -22,10 +22,6 @@ type Props = TutorialsProps;
 type State = TutorialsState;
 
 const Container = styled('div', (props: ThemeProps) => ({
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  justifyContent: 'center',
   width: '100%',
   height: '100%',
   backgroundColor: props.theme.backgroundColor,
@@ -44,14 +40,29 @@ const cardContainerPadding = () => {
 const VideoContainer = styled('div', (props: ThemeProps) => ({
   className: 'video-container',
   display: 'flex',
+  flexGrow: 0,
+  flexShrink: 0,
   flexDirection: 'column',
   alignItems: 'center',
   justifyContent: 'center',
   width: (window.innerWidth < window.innerHeight * 0.89) ? '80vw' : '89vh',
+  maxWidth: (window.innerWidth < window.innerHeight * 0.89) ? '89vh' : '80vw',
   height: (window.innerWidth < window.innerHeight * 0.89) ? '45vw' : '50vh',
+  maxHeight: (window.innerWidth < window.innerHeight * 0.89) ? '50vh' : '45vw',
   padding: '20px',
   backgroundColor: props.theme.backgroundColor,
   color: props.theme.color,
+}));
+
+const TutorialsContainer = styled("div", (props: ThemeProps) => ({
+  backgroundColor: props.theme.backgroundColor,
+  width: '100vw',
+  height: 'calc(100vh - 48px)',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  overflow: 'auto',
 }));
 
 const CardContainer = styled('div', (props: ThemeProps) => ({
@@ -59,11 +70,14 @@ const CardContainer = styled('div', (props: ThemeProps) => ({
   flexWrap: 'wrap',
   flexDirection: 'row',
   justifyContent: 'center',
+  alignContent: 'flex-start',
   paddingLeft: `${cardContainerPadding()}px`,
   paddingRight: `${cardContainerPadding()}px`,
-  backgroundColor: props.theme.backgroundColor,
+  height: '100%',
   width: '100%',
-  height: '100vh',
+  rowGap: '20px',
+  flexGrow: 1,
+  flexShrink: 1,
 }));
 
 class Tutorials extends React.Component<Props, State> {
@@ -92,36 +106,39 @@ class Tutorials extends React.Component<Props, State> {
     return (
       <Container style={style} theme={theme}>
         <MainMenu theme={theme}/>
-        {
-          (this.state.selected !== '') 
-            ? (
-              <VideoContainer theme={theme}>
-                <IFrame theme={theme} src={this.state.selected}/>
-              </VideoContainer>
-            ) : <></>
-        }
-        <CardContainer style={style} theme={theme}>
+        <TutorialsContainer style={style} theme={theme}>
           {
-            tutorialList.map((tutorial, index) => {
-              return (
-                <Card
-                  theme={theme}
-                  title={tutorial.title}
-                  description={tutorial.description}
-                  src={tutorial.src}
-                  backgroundImage={tutorial.backgroundImage}
-                  backgroundColor={tutorial.backgroundColor}
-                  backgroundPosition={tutorial.backgroundPosition}
-                  backgroundSize={tutorial.backgroundSize}
-                  hoverBackgroundSize={tutorial.hoverBackgroundSize}
-                  onSelect={this.onSelect_}
-                  index={index}
-                  key={index}
-                />
-              );
-            })
+            (this.state.selected !== '') 
+              ? (
+                <VideoContainer theme={theme}>
+                  <IFrame theme={theme} src={this.state.selected}/>
+                </VideoContainer>
+              ) : <></>
           }
-        </CardContainer>
+          <CardContainer style={style} theme={theme}>
+            {
+              tutorialList.map((tutorial, index) => {
+                return (
+                  <Card
+                    theme={theme}
+                    title={tutorial.title}
+                    description={tutorial.description}
+                    src={tutorial.src}
+                    backgroundImage={tutorial.backgroundImage}
+                    backgroundColor={tutorial.backgroundColor}
+                    backgroundPosition={tutorial.backgroundPosition}
+                    backgroundSize={tutorial.backgroundSize}
+                    hoverBackgroundSize={tutorial.hoverBackgroundSize}
+                    onSelect={this.onSelect_}
+                    index={index}
+                    key={index}
+                  />
+                );
+              })
+            }
+          </CardContainer>
+        </TutorialsContainer>
+        <div/>
       </Container>
     );
   }
