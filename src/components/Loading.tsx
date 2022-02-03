a823629fdc290d3e023d854565a7cde4f154a8da
Jack Williams <j.william@ou.edu>
2022-02-03 16:42:07 -0600

    moved Loading to be a component

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import ReactLoading from 'react-loading';
 import { styled } from 'styletron-react';
-import { ThemeProps } from './components/theme';
 
 export interface LoadingProps {
   message?: string
