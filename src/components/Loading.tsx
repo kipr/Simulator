d2dab5364e06c0532d1edd2c972537f63687a331
Jack Williams <j.william@ou.edu>
2022-02-07 20:56:40 -0600

    Added error text and some additional styling

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -4,8 +4,10 @@ import { styled } from 'styletron-react';
 
 export interface LoadingProps {
   message?: string
+  errorMessage?: string
   color?: string
-  width?: number
+  width?: number | string // number of pixels in width, or string width. Default 150
+  height?: number // number of pixels in height. Default 50
   type?: LoadingType
 }
 
@@ -15,12 +17,18 @@ const LoadingPage = styled('div', {
   alignItems: 'center',
   justifyContent: 'center',
   textAlign: 'center',
-  paddingTop: '35vh',
+  paddingTop: '30vh',
 });
 
 const LoadingText = styled('div', {
   fontSize: '32px',
 });
+
+const LoadingErrorText = styled('div', (props: LoadingProps) => ({
+  paddingTop: props.height ? 2 * props.height : '100px',
+  fontSize: '20px',
+  whiteSpace: 'pre-wrap'
+}));
 
 const Loading: React.FunctionComponent<LoadingProps> = (props) => {
   return (
@@ -31,8 +39,12 @@ const Loading: React.FunctionComponent<LoadingProps> = (props) => {
       <ReactLoading 
         type={props.type ? props.type : 'balls'}
         width={props.width ? props.width : 150}
+        height={props.height ? props.height : 50}
         color={props.color ? props.color : '#4b64ad'}
       />
+      <LoadingErrorText height={props.height}>
+        {props.errorMessage ? props.errorMessage : null}
+      </LoadingErrorText>
     </LoadingPage>
   );
 };
