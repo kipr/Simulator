729d23e075f6e1df6e2b644d13a4784cfad41c2d
Nafis Zaman <nafzam@outlook.com>
2022-02-12 11:48:31 -0800

    Fix robot position in challenges 17 and 17B

--- src/scenes/jbc17.ts
+++ src/scenes/jbc17.ts
@@ -1,4 +1,5 @@
 import Scene from "../state/State/Scene";
+import { Distance } from "../util";
 
 import { createBaseSceneSurfaceB } from './jbcBase';
 
@@ -8,4 +9,16 @@ export const JBC_17: Scene = {
   ...baseScene,
   name: 'JBC 17',
   description: 'Junior Botball Challenge 17: Walk the Line',
+  // Start the robot on the black tape
+  robot: {
+    ...baseScene.robot,
+    origin: {
+      ...baseScene.robot.origin,
+      position: {
+        x: Distance.centimeters(16.5),
+        y: Distance.centimeters(111),
+        z: Distance.centimeters(-50),
+      },
+    }
+  },
 };
--- src/scenes/jbc17b.ts
+++ src/scenes/jbc17b.ts
@@ -1,4 +1,5 @@
 import Scene from "../state/State/Scene";
+import { Distance } from "../util";
 
 import { createBaseSceneSurfaceB } from './jbcBase';
 
@@ -8,4 +9,16 @@ export const JBC_17B: Scene = {
   ...baseScene,
   name: 'JBC 17B',
   description: 'Junior Botball Challenge 17: Walk the Line II',
+  // Start the robot on the black tape
+  robot: {
+    ...baseScene.robot,
+    origin: {
+      ...baseScene.robot.origin,
+      position: {
+        x: Distance.centimeters(16.5),
+        y: Distance.centimeters(111),
+        z: Distance.centimeters(-50),
+      },
+    }
+  },
 };
