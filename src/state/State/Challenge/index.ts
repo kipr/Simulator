ebcb6d293a822f96ed4c374bab53312fb6f1e603
Braden McDorman <bmcdorman@gmail.com>
2022-12-07 09:28:38 -0500

    Add helper types

--- src/state/State/Challenge/index.ts
+++ src/state/State/Challenge/index.ts
@@ -1,6 +1,7 @@
 import Dict from '../../../Dict';
 import LocalizedString from '../../../util/LocalizedString';
 import Reference from '../../../util/Reference';
+import Async from '../Async';
 import Scene from '../Scene';
 import Event from './Event';
 import Expr from './Expr';
@@ -16,5 +17,12 @@ interface Challenge {
 
   sceneId: string;
 }
+
+export interface ChallengeBrief {
+  name: LocalizedString;
+  description: LocalizedString;
+}
+
+export type AsyncChallenge = Async<ChallengeBrief, Challenge>;
 
 export default Challenge;
