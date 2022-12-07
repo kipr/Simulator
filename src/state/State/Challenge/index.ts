4a257213628a951211da17dc4eeaea8df5e4fa49
Braden McDorman <bmcdorman@gmail.com>
2022-12-07 12:22:09 -0500

    Add test challenge and helpers

--- src/state/State/Challenge/index.ts
+++ src/state/State/Challenge/index.ts
@@ -1,3 +1,4 @@
+import Author from '../../../db/Author';
 import Dict from '../../../Dict';
 import LocalizedString from '../../../util/LocalizedString';
 import Reference from '../../../util/Reference';
@@ -9,6 +10,7 @@ import Expr from './Expr';
 interface Challenge {
   name: LocalizedString;
   description: LocalizedString;
+  author: Author;
 
   events: Dict<Event>;
 
@@ -21,8 +23,34 @@ interface Challenge {
 export interface ChallengeBrief {
   name: LocalizedString;
   description: LocalizedString;
+  author: Author;
+}
+
+export namespace ChallengeBrief {
+  export const fromChallenge = (challenge: Challenge): ChallengeBrief => ({
+    name: challenge.name,
+    description: challenge.description,
+    author: challenge.author,
+  });
 }
 
 export type AsyncChallenge = Async<ChallengeBrief, Challenge>;
+
+export namespace AsyncChallenge {
+  export const unloaded = (brief: ChallengeBrief): AsyncChallenge => ({
+    type: Async.Type.Unloaded,
+    brief,
+  });
+
+  export const loaded = (challenge: Challenge): AsyncChallenge => ({
+    type: Async.Type.Loaded,
+    brief: {
+      name: challenge.name,
+      description: challenge.description,
+      author: challenge.author,
+    },
+    value: challenge,
+  });
+}
 
 export default Challenge;
