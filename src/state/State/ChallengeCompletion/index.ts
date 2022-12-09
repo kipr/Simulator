1b02e4843f4efde0c8f127a1744150b7aeabf378
Braden McDorman <bmcdorman@gmail.com>
2022-12-09 12:39:26 -0500

    Add helper types

--- src/state/State/ChallengeCompletion/index.ts
+++ src/state/State/ChallengeCompletion/index.ts
@@ -1,5 +1,6 @@
 import Dict from '../../../Dict';
 import Patch from '../../../util/Patch';
+import Async from '../Async';
 import Scene from '../Scene';
 import PredicateCompletion from './PredicateCompletion';
 
@@ -8,6 +9,28 @@ interface ChallengeCompletion {
   events: Dict<boolean>;
   success?: PredicateCompletion;
   failure?: PredicateCompletion;
+}
+
+export interface ChallengeCompletionBrief {
+}
+
+export namespace ChallengeCompletionBrief {
+}
+
+export type AsyncChallengeCompletion = Async<ChallengeCompletionBrief, ChallengeCompletion>;
+
+export namespace AsyncChallenge {
+  export const unloaded = (brief: ChallengeCompletionBrief): AsyncChallengeCompletion => ({
+    type: Async.Type.Unloaded,
+    brief,
+  });
+
+  export const loaded = (challenge: ChallengeCompletion): AsyncChallengeCompletion => ({
+    type: Async.Type.Loaded,
+    brief: {
+    },
+    value: challenge,
+  });
 }
 
 export default ChallengeCompletion;
