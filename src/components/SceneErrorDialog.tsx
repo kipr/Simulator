b896e5acd101d6c31763f034338d8c02172fb799
Braden McDorman <bmcdorman@gmail.com>
2023-01-20 07:49:06 -0500

    Fix sprintf format string

--- src/components/SceneErrorDialog.tsx
+++ src/components/SceneErrorDialog.tsx
@@ -41,7 +41,7 @@ class SceneErrorDialog extends React.Component<Props> {
     return (
       <Dialog
         theme={theme}
-        name={LocalizedString.lookup(Dict.map(tr('Error %s'), (str: string) => sprintf(str, error.code)), locale)}
+        name={LocalizedString.lookup(Dict.map(tr('Error %d'), (str: string) => sprintf(str, error.code)), locale)}
         onClose={onClose}
       >
         <Container theme={theme}>
