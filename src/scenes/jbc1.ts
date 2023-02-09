09ab7f1f88fcd00f9b7ad7c79949ddb446c90aa4
Erin Harrington <erinharrington12@yahoo.com>
2023-02-09 11:04:23 -0600

    removed more challenge stuff

--- src/scenes/jbc1.ts
+++ src/scenes/jbc1.ts
@@ -1,6 +1,5 @@
-import Scene from "../state/State/Scene";
+import Scene from '../state/State/Scene';
 import LocalizedString from '../util/LocalizedString';
-import { Distance } from "../util";
 
 import { createCanNode, createBaseSceneSurfaceA } from './jbcBase';
 
@@ -9,16 +8,12 @@ const baseScene = createBaseSceneSurfaceA();
 export const JBC_1: Scene = {
   ...baseScene,
   name: { [LocalizedString.EN_US]: 'JBC 1' },
-  description: { [LocalizedString.EN_US]: `Junior Botball Challenge 1: Tag, You're It!` },
+  description: {
+    [LocalizedString.EN_US]: `Junior Botball Challenge 1: Tag, You're It!`,
+  },
 
   nodes: {
     ...baseScene.nodes,
-    
-    can9: {
-      ...createCanNode(9, { x: Distance.centimeters(0), y: Distance.centimeters(0), z: Distance.centimeters(85.4) }),
-      scriptIds: [ 'robotTouches' ]
-    }
-    
-  }
-  
+    can9: createCanNode(9),
+  },
 };
