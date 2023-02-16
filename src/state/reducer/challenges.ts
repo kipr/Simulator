c663f4c3ce6035d191d430136d17e5df97c6ab8b
Erin Harrington <erinharrington12@yahoo.com>
2023-02-16 11:05:09 -0600

    Finished JBC10 logic and testing

--- src/state/reducer/challenges.ts
+++ src/state/reducer/challenges.ts
@@ -26,6 +26,7 @@ import jbc7 from "../../challenges/jbc7";
 import jbc7b from "../../challenges/jbc7b";
 import jbc8 from "../../challenges/jbc8";
 import jbc8b from "../../challenges/jbc8b";
+import jbc10 from "../../challenges/jbc10";
 
 export namespace ChallengesAction {
   export interface LoadChallenge {
@@ -189,6 +190,10 @@ const DEFAULT_CHALLENGES: Challenges = {
   'jbc8b': Async.loaded({
     value: jbc8b,
     brief: ChallengeBrief.fromChallenge(jbc8b),
+  }),
+  'jbc10': Async.loaded({
+    value: jbc10,
+    brief: ChallengeBrief.fromChallenge(jbc10),
   }),
 };
 
