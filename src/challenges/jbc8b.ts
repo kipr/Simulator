c2721cdae0cfee316034c9510824ccc9ce29e074
Erin Harrington <erinharrington12@yahoo.com>
2023-02-16 08:58:03 -0600

    Finished JBC8B logic and testing

---
+++ src/challenges/jbc8b.ts
@@ -0,0 +1,117 @@
+import Author from '../db/Author';
+import Challenge from '../state/State/Challenge';
+import Expr from '../state/State/Challenge/Expr';
+import LocalizedString from '../util/LocalizedString';
+
+export default {
+  name: { [LocalizedString.EN_US]: 'JBC Challenge 8B' },
+  description: {
+    [LocalizedString.EN_US]: 'Junior Botball Challenge 8B: Serpentine Jr.',
+  },
+  author: {
+    type: Author.Type.Organization,
+    id: 'kipr',
+  },
+  code: {
+    c: `#include <kipr/botball.h>`,
+    cpp: `#include <kipr/botball.h>`,
+    python: `from kipr import botball`,
+  },
+  defaultLanguage: 'c',
+  events: {
+    start: {
+      name: { [LocalizedString.EN_US]: 'Robot Begins In start' },
+      description: { [LocalizedString.EN_US]: 'Robot begins in starting box' },
+    },
+    passed1: {
+      name: { [LocalizedString.EN_US]: 'Robot Touched Circle 1' },
+      description: { [LocalizedString.EN_US]: 'Robot passed through circle 1' },
+    },
+    passed2: {
+      name: { [LocalizedString.EN_US]: 'Robot Touched Circle 2' },
+      description: { [LocalizedString.EN_US]: 'Robot passed through circle 2' },
+    },
+    passed3: {
+      name: { [LocalizedString.EN_US]: 'Robot Touched Circle 3' },
+      description: { [LocalizedString.EN_US]: 'Robot passed through circle 3' },
+    },
+    passed4: {
+      name: { [LocalizedString.EN_US]: 'Robot Touched Circle 4' },
+      description: { [LocalizedString.EN_US]: 'Robot passed through circle 4' },
+    },
+    passed5: {
+      name: { [LocalizedString.EN_US]: 'Robot Touched Circle 5' },
+      description: { [LocalizedString.EN_US]: 'Robot passed through circle 5' },
+    },
+  },
+  success: {
+    exprs: {
+      //Start Box Event
+      start: {
+        type: Expr.Type.Event,
+        eventId: 'start',
+      },
+      startOnce: {
+        type: Expr.Type.Once,
+        argId: 'start',
+      },
+
+      //Passed Through Circles Events
+      passed1: {
+        type: Expr.Type.Event,
+        eventId: 'passed1',
+      },
+      passed1Once: {
+        type: Expr.Type.Once,
+        argId: 'passed1',
+      },
+
+      passed2: {
+        type: Expr.Type.Event,
+        eventId: 'passed2',
+      },
+      passed2Once: {
+        type: Expr.Type.Once,
+        argId: 'passed2',
+      },
+      passed3: {
+        type: Expr.Type.Event,
+        eventId: 'passed3',
+      },
+      passed3Once: {
+        type: Expr.Type.Once,
+        argId: 'passed3',
+      },
+      passed4: {
+        type: Expr.Type.Event,
+        eventId: 'passed4',
+      },
+      passed4Once: {
+        type: Expr.Type.Once,
+        argId: 'passed4',
+      },
+      passed5: {
+        type: Expr.Type.Event,
+        eventId: 'passed5',
+      },
+      passed5Once: {
+        type: Expr.Type.Once,
+        argId: 'passed5',
+      },
+
+
+      passedSerpentine: {
+        type: Expr.Type.And,
+        argIds: ["passed1Once", "passed2Once","passed3Once", "passed4Once", "passed5Once"],
+      },
+
+      // Success logic
+      completion: {
+        type: Expr.Type.And,
+        argIds: ['startOnce', 'passedSerpentine'],
+      },
+    },
+    rootId: 'completion',
+  },
+  sceneId: 'jbc8b',
+} as Challenge;
