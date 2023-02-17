8ea05e9a3a5228583247719e28317ced550d4258
Erin Harrington <erinharrington12@yahoo.com>
2023-02-17 10:53:57 -0600

    Started JBC5 logic and testing. PWM has a bug so will continue when fixed

--- src/scenes/jbc5.ts
+++ src/scenes/jbc5.ts
@@ -1,15 +1,12 @@
-import Scene from "../state/State/Scene";
-import LocalizedString from "../util/LocalizedString";
-import Script from "../state/State/Scene/Script";
-import { createCanNode, createBaseSceneSurfaceA } from "./jbcBase";
-import { Color } from "../state/State/Scene/Color";
-import { Distance } from "../util";
-import { ReferenceFrame, Rotation, Vector3 } from "../unit-math";
+import Scene from '../state/State/Scene';
+import LocalizedString from '../util/LocalizedString';
+import Script from '../state/State/Scene/Script';
+import { createCanNode, createBaseSceneSurfaceA } from './jbcBase';
+import { Color } from '../state/State/Scene/Color';
+import { Distance } from '../util';
+import { ReferenceFrame, Rotation, Vector3 } from '../unit-math';
 
 const baseScene = createBaseSceneSurfaceA();
-
-
-
 
 const leftStartBox = `
 
@@ -22,33 +19,53 @@ scene.addOnIntersectionListener('robot', (type, otherNodeId) => {
 }, 'startBox');
 `;
 
-
-
-
 const goingBackwards = `
-
+let degrees = [];
 scene.addOnRenderListener(() => {
+  
   const EULER_IDENTITY = Rotation.Euler.identity();
   const xAngle = (nodeId) => 180 / Math.PI * Math.acos(Vector3.dot(Vector3.applyQuaternion(Vector3.X, Rotation.toRawQuaternion(scene.nodes[nodeId].origin.orientation || EULER_IDENTITY)), Vector3.X));
-  
-  console.log("Robot orientation: " + xAngle('robot')); 
+  let angle = xAngle('robot');
+  console.log(angle);
+  if(angle >= 179 && angle <= 180){
+    degrees[0] = 0;
+    degrees[1] = 180;
+    
+  }
+  if(angle <= 1 && (degrees[1] == 180)&& ((scene.nodes['robot'].state.motors[0].pwm > scene.nodes['robot'].state.motors[3].pwm) || scene.nodes['robot'].state.motors[3].direction === 2)){
+    scene.setChallengeEventValue('counterClockwise360', true);
+    degrees.pop();
+    degrees.pop();
+  }
+  else if (angle <=1 && (degrees[1] == 180) && (scene.nodes['robot'].state.motors[3].pwm > scene.nodes['robot'].state.motors[0].pwm) && scene.nodes['robot'].state.motors[3].direction != 2 )
+  {
+    scene.setChallengeEventValue('clockwise360', true);
+    degrees.pop();
+    degrees.pop();
+  }
+  console.log("0 PDM: " + scene.nodes['robot'].state.motors[0].pwm);
+  console.log("3 PDM: " + scene.nodes['robot'].state.motors[3].pwm);
+  console.log("0 direction: " + scene.nodes['robot'].state.motors[0].direction);
+  console.log("3 direction: " + scene.nodes['robot'].state.motors[3].direction);
+  console.log(degrees) ;
 });
 
 `;
 
-
 export const JBC_5: Scene = {
   ...baseScene,
   name: { [LocalizedString.EN_US]: 'JBC 5' },
-  description: { [LocalizedString.EN_US]: 'Junior Botball Challenge 5: Dance Party' },
+  description: {
+    [LocalizedString.EN_US]: 'Junior Botball Challenge 5: Dance Party',
+  },
   scripts: {
-    goingBackwards: Script.ecmaScript("Going Backwards", goingBackwards),
-    leftStartBox: Script.ecmaScript("Robot Reentered Start", leftStartBox),
+    goingBackwards: Script.ecmaScript('Going Backwards', goingBackwards),
+    leftStartBox: Script.ecmaScript('Robot Reentered Start', leftStartBox),
   },
   geometry: {
     ...baseScene.geometry,
     mainSurface_geom: {
-      type: "box",
+      type: 'box',
       size: {
         x: Distance.meters(3.54),
         y: Distance.centimeters(0.1),
@@ -56,7 +73,7 @@ export const JBC_5: Scene = {
       },
     },
     startBox_geom: {
-      type: "box",
+      type: 'box',
       size: {
         x: Distance.meters(3.54),
         y: Distance.centimeters(0.1),
@@ -67,9 +84,9 @@ export const JBC_5: Scene = {
   nodes: {
     ...baseScene.nodes,
     mainSurface: {
-      type: "object",
-      geometryId: "mainSurface_geom",
-      name: { [LocalizedString.EN_US]: "Mat Surface" },
+      type: 'object',
+      geometryId: 'mainSurface_geom',
+      name: { [LocalizedString.EN_US]: 'Mat Surface' },
       visible: false,
       origin: {
         position: {
@@ -79,17 +96,17 @@ export const JBC_5: Scene = {
         },
       },
       material: {
-        type: "basic",
+        type: 'basic',
         color: {
-          type: "color3",
+          type: 'color3',
           color: Color.rgb(0, 0, 0),
         },
       },
     },
     startBox: {
-      type: "object",
-      geometryId: "startBox_geom",
-      name: { [LocalizedString.EN_US]: "Start Box" },
+      type: 'object',
+      geometryId: 'startBox_geom',
+      name: { [LocalizedString.EN_US]: 'Start Box' },
       visible: false,
       origin: {
         position: {
@@ -99,13 +116,12 @@ export const JBC_5: Scene = {
         },
       },
       material: {
-        type: "pbr",
+        type: 'pbr',
         emissive: {
-          type: "color3",
+          type: 'color3',
           color: Color.rgb(255, 255, 255),
         },
       },
     },
- 
   },
 };
