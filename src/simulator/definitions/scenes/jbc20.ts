04f57952e109f350853c1644fb0177e34d447c89
Erin Harrington <erinharrington12@yahoo.com>
2023-02-22 13:12:51 -0600

    Finished JBC20 logic with testing

--- src/simulator/definitions/scenes/jbc20.ts
+++ src/simulator/definitions/scenes/jbc20.ts
@@ -1,11 +1,40 @@
-import Scene from "../../../state/State/Scene";
+import Scene from '../../../state/State/Scene';
 import { ReferenceFramewUnits } from '../../../util/math/unitMath';
-import { Distance, Mass } from "../../../util";
+import { Distance, Mass } from '../../../util';
 import LocalizedString from '../../../util/LocalizedString';
-
+import Script from '../../../state/State/Scene/Script';
 import { createBaseSceneSurfaceA, createCanNode } from './jbcBase';
-
 import tr from '@i18n';
+
+const uprightCans = `
+// When a can is standing upright on the ream, the upright condition is met.
+
+
+scene.onBind = nodeId => {
+  const EULER_IDENTITY = Rotation.Euler.identity();
+  const yAngle = (nodeId) => 180 / Math.PI * Math.acos(Vector3.dot(Vector3.applyQuaternion(Vector3.Y, Rotation.toRawQuaternion(scene.nodes[nodeId].origin.orientation || EULER_IDENTITY)), Vector3.Y));
+  
+  scene.addOnRenderListener(() => {
+   
+    const canUpright2 = yAngle('can2') < 5;
+    const canUpright9 = yAngle('can9') < 5;
+    const canUpright10 = yAngle('can10') < 5;
+    const canUpright12 = yAngle('can12') < 5;
+    scene.setChallengeEventValue('can2Upright', canUpright2);
+    scene.setChallengeEventValue('can9Upright', canUpright9);
+    scene.setChallengeEventValue('can10Upright', canUpright10);
+    scene.setChallengeEventValue('can12Upright', canUpright12);
+    
+  });
+  scene.addOnIntersectionListener(nodeId, (type, otherNodeId) => {
+    const visible = type === 'start';
+    scene.setChallengeEventValue(nodeId +'Intersects', visible); 
+    
+  }, 'ream');
+
+};
+  
+`;
 
 const baseScene = createBaseSceneSurfaceA();
 
@@ -13,7 +42,7 @@ const ROBOT_ORIGIN: ReferenceFramewUnits = {
   ...baseScene.nodes['robot'].origin,
   position: {
     ...baseScene.nodes['robot'].origin.position,
-    x: Distance.centimeters(18)
+    x: Distance.centimeters(18),
   },
 };
 
@@ -27,22 +56,39 @@ const REAM_ORIGIN: ReferenceFramewUnits = {
 
 export const JBC_20: Scene = {
   ...baseScene,
-  name: tr('JBC 20'),
-  description: tr('Junior Botball Challenge 20: Rescue the Cans'),
+  name: { [LocalizedString.EN_US]: 'JBC 20' },
+  description: {
+    [LocalizedString.EN_US]: `Junior Botball Challenge 20: Rescue the Cans`,
+  },
+  scripts: {
+    uprightCans: Script.ecmaScript('Upright Cans', uprightCans),
+  },
   nodes: {
     ...baseScene.nodes,
     // The normal starting position of the robot doesn't leave room for the paper ream in the starting box
     // Start the robot on the left side so that a ream fits on the right side
-    'robot': {
+    robot: {
       ...baseScene.nodes['robot'],
       startingOrigin: ROBOT_ORIGIN,
-      origin: ROBOT_ORIGIN
+      origin: ROBOT_ORIGIN,
     },
-    'can2': createCanNode(2),
-    'can9': createCanNode(9),
-    'can10': createCanNode(10),
-    'can12': createCanNode(12),
-    'ream': {
+    can2: {
+      ...createCanNode(2),
+      scriptIds: ['uprightCans'],
+    },
+    can9: {
+      ...createCanNode(9),
+      scriptIds: ['uprightCans'],
+    },
+    can10: {
+      ...createCanNode(10),
+      scriptIds: ['uprightCans'],
+    },
+    can12: {
+      ...createCanNode(12),
+      scriptIds: ['uprightCans'],
+    },
+    ream: {
       type: 'from-jbc-template',
       templateId: 'ream',
       name: tr('Paper Ream'),
