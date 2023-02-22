5459973bfb9ca9436b645505bf7ddcf270a71cf9
Erin Harrington <erinharrington12@yahoo.com>
2023-02-22 16:01:33 -0600

    Started JBC21

--- src/state/reducer/challenges.ts
+++ src/state/reducer/challenges.ts
@@ -33,6 +33,7 @@ import jbc15b from "../../challenges/jbc15b";
 import jbc17 from "../../challenges/jbc17";
 import jbc19 from "../../challenges/jbc19";
 import jbc20 from "../../challenges/jbc20";
+import jbc21 from "../../challenges/jbc21";
 
 export namespace ChallengesAction {
   export interface LoadChallenge {
@@ -224,6 +225,10 @@ const DEFAULT_CHALLENGES: Challenges = {
   'jbc20': Async.loaded({
     value: jbc20,
     brief: ChallengeBrief.fromChallenge(jbc20),
+  }),
+  'jbc21': Async.loaded({
+    value: jbc21,
+    brief: ChallengeBrief.fromChallenge(jbc21),
   }),
 };
 
