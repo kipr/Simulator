0c51a16ad53e69774b00ab4e8839dd540eee5e82
Erin Harrington <erinharrington12@yahoo.com>
2023-03-02 09:29:04 -0600

    Working on JBC22

--- src/simulator/definitions/challenges/jbc22.ts
+++ src/simulator/definitions/challenges/jbc22.ts
@@ -26,14 +26,23 @@ export default {
       name: { [LocalizedString.EN_US]: 'Robot Left Start' },
       description: { [LocalizedString.EN_US]: 'Robot left starting box' },
     },
+
+    canStacked: {
+      name: { [LocalizedString.EN_US]: 'One Can Stacked' },
+      description: { [LocalizedString.EN_US]: 'One can is stacked on another' },
+    },
  
+    robotTouchCan: {
+      name: { [LocalizedString.EN_US]: 'Robot Touching Can' },
+      description: { [LocalizedString.EN_US]: 'Robot is touching a can' },
+    },
 
   },
   success: {
     exprs: {
 
 
-      //Upright Events
+      //Startbox Events
       leaveStartBox: {
         type: Expr.Type.Event,
         eventId: 'leaveStartBox',
@@ -42,10 +51,25 @@ export default {
         type: Expr.Type.Once,
         argId: 'leaveStartBox',
       },
+
+      //Can Events
+      canStacked: {
+        type: Expr.Type.Event,
+        eventId: 'canStacked',
+      },
+      robotTouchCan: {
+        type: Expr.Type.Event,
+        eventId: 'robotTouchCan',
+      },
+      robotTouchCanNot: {
+        type: Expr.Type.Not,
+        argId: 'robotTouchCan',
+      },
+
    
       completion: {
         type: Expr.Type.And,
-        argIds: ['leaveStartBoxOnce'],
+        argIds: ['leaveStartBoxOnce', 'canStacked', 'robotTouchCanNot'],
       },
     },
     rootId: 'completion',
