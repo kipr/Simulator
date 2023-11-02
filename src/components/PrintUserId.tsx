b16cf8c284362a96d95e36853ce06a6a0e0f6785
Braden McDorman <bmcdorman@gmail.com>
2023-11-02 08:45:11 -0700

    dispatch example

--- src/components/PrintUserId.tsx
+++ src/components/PrintUserId.tsx
@@ -3,17 +3,24 @@ import * as React from 'react';
 import { connect } from 'react-redux';
 
 import { State as ReduxState } from '../state';
+import { UserAction } from 'state/reducer';
 
 export interface PrintUserIdProps {
   userId: string;
+  setUserId: (userId?: string) => void;
 }
 
 const PrintUserId = (props: PrintUserIdProps) => (
   <div>
     {props.userId}
+    <button onClick={() => props.setUserId("1")}>Make the User ID "1"</button>
   </div>
 );
 
+// connect(GETS: state => props, SETS: dispatch => props)
+
 export default connect((state: ReduxState) => ({
   userId: state.user.userId,
+}), dispatch => ({
+  setUserId: (userId?: string) => dispatch(UserAction.setUserId(userId)),
 }))(PrintUserId);
