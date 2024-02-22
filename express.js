d59cc605cc5c52b6f6fd9f482e26ed03eb964ce4
Tim Corbly <tcorbly@kipr.org>
2024-02-22 14:59:32 -0700

    Add rate limiter

--- express.js
+++ express.js
@@ -21,6 +21,16 @@ try {
   throw e;
 }
 
+// set up rate limiter: maximum of 100 requests per 15 minute
+var RateLimit = require('express-rate-limit');
+var limiter = RateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // max 100 requests per windowMs
+});
+
+// apply rate limiter to all requests
+app.use(limiter);
+
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
