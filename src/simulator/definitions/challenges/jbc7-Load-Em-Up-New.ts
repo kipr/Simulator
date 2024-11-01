a446ff466288db417cd886d314a0a4e13ba7c329
Erin <erinharrington12@yahoo.com>
2024-11-01 16:31:40 -0500

    JBC7 Load Em Up New challenge and scene

--- src/simulator/definitions/challenges/jbc7-Load-Em-Up-New.ts
+++ src/simulator/definitions/challenges/jbc7-Load-Em-Up-New.ts
@@ -1,98 +1,133 @@
-import Author from "../../../db/Author";
-import Challenge from "../../../state/State/Challenge";
-import Expr from "../../../state/State/Challenge/Expr";
-import LocalizedString from "../../../util/LocalizedString";
+import Author from '../../../db/Author';
+import Challenge from '../../../state/State/Challenge';
+import Expr from '../../../state/State/Challenge/Expr';
+import LocalizedString from '../../../util/LocalizedString';
 import ProgrammingLanguage from "../../../programming/compiler/ProgrammingLanguage";
 
 export default {
-  name: { [LocalizedString.EN_US]: "JBC Challenge 7" },
+  name: { [LocalizedString.EN_US]: 'JBC Challenge 7' },
   description: {
     [LocalizedString.EN_US]: `Junior Botball Challenge 7: Load 'Em Up`,
   },
   author: {
     type: Author.Type.Organization,
-    id: "kipr",
+    id: 'kipr',
   },
   code: {
     'c': ProgrammingLanguage.DEFAULT_CODE.c,
     'cpp': ProgrammingLanguage.DEFAULT_CODE.cpp,
     'python': ProgrammingLanguage.DEFAULT_CODE.python,
   },
-  defaultLanguage: "c",
+  defaultLanguage: 'c',
   events: {
+
+    leaveStartBox: {
+      name: { [LocalizedString.EN_US]: 'Robot Left Start' },
+      description: { [LocalizedString.EN_US]: 'Robot left starting box' },
+    },
+
     can2Upright: {
-      name: { [LocalizedString.EN_US]: "Can 2 Upright" },
-      description: { [LocalizedString.EN_US]: "Can 2 upright" },
+      name: { [LocalizedString.EN_US]: 'Can 2 Upright' },
+      description: { [LocalizedString.EN_US]: 'Can 2 upright' },
     },
     can9Upright: {
-      name: { [LocalizedString.EN_US]: "Can 9 Upright" },
-      description: { [LocalizedString.EN_US]: "Can 9 upright" },
+      name: { [LocalizedString.EN_US]: 'Can 9 Upright' },
+      description: { [LocalizedString.EN_US]: 'Can 9 upright' },
     },
     can10Upright: {
-      name: { [LocalizedString.EN_US]: "Can 10 Upright" },
-      description: { [LocalizedString.EN_US]: "Can 10 upright" },
+      name: { [LocalizedString.EN_US]: 'Can 10 Upright' },
+      description: { [LocalizedString.EN_US]: 'Can 10 upright' },
     },
 
     can2Intersects: {
-      name: { [LocalizedString.EN_US]: "Can 2 Intersects" },
-      description: { [LocalizedString.EN_US]: "Can 2 intersects green garage" },
+      name: { [LocalizedString.EN_US]: 'Can 2 Intersects Green Garage' },
+      description: { [LocalizedString.EN_US]: 'Can 2 intersects Green Garage' },
     },
     can9Intersects: {
-      name: { [LocalizedString.EN_US]: "Can 9 Intersects" },
-      description: { [LocalizedString.EN_US]: "Can 9 intersects blue garage" },
+      name: { [LocalizedString.EN_US]: 'Can 9 Intersects Blue Garage' },
+      description: { [LocalizedString.EN_US]: 'Can 9 intersects Blue Garage' },
+    },
+    can9IntersectsPurple: {
+      name: { [LocalizedString.EN_US]: 'Can 9 Intersects Purple Garage' },
+      description: { [LocalizedString.EN_US]: 'Can 9 intersects Purple Garage' },
     },
     can10Intersects: {
-      name: { [LocalizedString.EN_US]: "Can 10 Intersects" },
-      description: { [LocalizedString.EN_US]: "Can 10 intersects yellow garage" },
+      name: { [LocalizedString.EN_US]: 'Can 10 Intersects Yellow Garage' },
+      description: { [LocalizedString.EN_US]: 'Can 10 intersects Yellow Garage' },
     },
+
   },
   success: {
     exprs: {
+      // Start Box Events
+      leaveStartBox: {
+        type: Expr.Type.Event,
+        eventId: 'leaveStartBox',
+      },
+      leaveStartBoxOnce: {
+        type: Expr.Type.Once,
+        argId: 'leaveStartBox',
+      },
+
+
       // Upright Events
       can2Upright: {
         type: Expr.Type.Event,
-        eventId: "can2Upright",
+        eventId: 'can2Upright',
       },
       can9Upright: {
         type: Expr.Type.Event,
-        eventId: "can9Upright",
+        eventId: 'can9Upright',
       },
       can10Upright: {
         type: Expr.Type.Event,
-        eventId: "can10Upright",
+        eventId: 'can10Upright',
       },
-      cansUpright: {
-        type: Expr.Type.And,
-        argIds: ["can2Upright", "can9Upright", "can10Upright"],
-      },
-
 
       // Intersects Events
       can2Intersects: {
         type: Expr.Type.Event,
-        eventId: "can2Intersects",
+        eventId: 'can2Intersects',
       },
       can9Intersects: {
         type: Expr.Type.Event,
-        eventId: "can9Intersects",
+        eventId: 'can9Intersects',
       },
       can10Intersects: {
         type: Expr.Type.Event,
-        eventId: "can10Intersects",
+        eventId: 'can10Intersects',
       },
-      cansIntersects: {
+
+      // Upright and Intersects logic
+      can2UprightAndIntersects: {
         type: Expr.Type.And,
-        argIds: ["can2Intersects", "can9Intersects", "can10Intersects"],
+        argIds: ['can2Upright', 'can2Intersects'],
       },
+      can9UprightAndIntersects: {
+        type: Expr.Type.And,
+        argIds: ['can9Upright', 'can9Intersects'],
+      },
+      can10UprightAndIntersects: {
+        type: Expr.Type.And,
+        argIds: ['can10Upright', 'can10Intersects'],
+      },
+
+      // Final And Logic
+      cansUprightAndIntersects: {
+        type: Expr.Type.And,
+        argIds: ['can2UprightAndIntersects', 'can9UprightAndIntersects', 'can10UprightAndIntersects',],
+      },
+
 
 
       // Success logic
       completion: {
         type: Expr.Type.And,
-        argIds: ["cansUpright", "cansIntersects"],
+        argIds: ['leaveStartBoxOnce', 'cansUprightAndIntersects'],
       },
     },
-    rootId: "completion",
+    rootId: 'completion',
   },
-  sceneId: "jbc7",
+
+  sceneId: 'jbc7',
 } as Challenge;
--- src/simulator/definitions/scenes/jbc7-Load-Em-Up-New.ts
+++ src/simulator/definitions/scenes/jbc7-Load-Em-Up-New.ts
@@ -1,45 +1,58 @@
-import Scene from "../../../state/State/Scene";
-import LocalizedString from "../../../util/LocalizedString";
-import { createBaseSceneSurfaceA, createCanNode } from "./jbcBase";
-import { Color } from "../../../state/State/Scene/Color";
-import { Distance } from "../../../util";
-import Script from "../../../state/State/Scene/Script";
+import Scene from '../../../state/State/Scene';
+import { ReferenceFramewUnits, RotationwUnits } from '../../../util/math/unitMath';
+import { Distance, Angle } from '../../../util';
+import LocalizedString from '../../../util/LocalizedString';
+import Script from '../../../state/State/Scene/Script';
+import { canPositions, createBaseSceneSurfaceA, createBaseSceneSurfaceB, createCanNode } from './jbcBase';
+import { Color } from '../../../state/State/Scene/Color';
+import Robot from 'state/State/Robot';
+import Node from 'state/State/Scene/Node';
 
 const baseScene = createBaseSceneSurfaceA();
 
-const garageIntersects = `
+const leftStartBox = `
+
+scene.addOnIntersectionListener('robot', (type, otherNodeId) => {
+
+  if(scene.programStatus === 'running'){
+    scene.setChallengeEventValue('leaveStartBox', type === 'end');
+  }
+  
+}, 'startBox');
+`;
+
+
+const canIntersectsGarage = `
 const setNodeVisible = (nodeId, visible) => scene.setNode(nodeId, {
   ...scene.nodes[nodeId],
   visible
 });
 
-// When the can (can2) is intersecting the green garage, the garage glows
 
+// When the can (can2) is intersecting the green garage, the garage glows
 scene.addOnIntersectionListener('can2', (type, otherNodeId) => {
-  console.log('Can 2 placed!', type, otherNodeId);
   const visible = type === 'start';
   scene.setChallengeEventValue('can2Intersects', visible);
   setNodeVisible('greenGarage', visible);
 }, 'greenGarage');
 
 // When the can (can9) is intersecting the blue garage, the garage glows
-
 scene.addOnIntersectionListener('can9', (type, otherNodeId) => {
-  console.log('Can 9 placed!', type, otherNodeId);
   const visible = type === 'start';
   scene.setChallengeEventValue('can9Intersects', visible);
-  setNodeVisible('circle9', visible);
-}, 'circle9');
+  setNodeVisible('blueGarage', visible);
+}, 'blueGarage');
 
 
-// // When the can (can10) is intersecting the yellow garage, the garage glows
 
-// scene.addOnIntersectionListener('can12', (type, otherNodeId) => {
-//   console.log('Can 12 placed!', type, otherNodeId);
-//   const visible = type === 'start';
-//   scene.setChallengeEventValue('can12Intersects', visible);
-//   setNodeVisible('circle12', visible);
-// }, 'circle12');
+
+scene.addOnIntersectionListener('can10', (type, otherNodeId) => {
+  const visible = type === 'start';
+  scene.setChallengeEventValue('can10Intersects', visible);
+  setNodeVisible('yellowGarage', visible);
+}, 'yellowGarage');
+
+
 `;
 
 
@@ -49,159 +62,297 @@ const uprightCans = `
 // let startTime = Date.now();
 const EULER_IDENTITY = RotationwUnits.EulerwUnits.identity();
 // const startingOrientationInv = (nodeId) => Quaternion.inverse(RotationwUnits.toRawQuaternion(scene.nodes[nodeId].startingOrigin.orientation || EULER_IDENTITY));
-const yAngle = (nodeId) => 180 / Math.PI * Math.acos(Vector3wUnits.dot(Vector3wUnits.applyQuaternion(Vector3wUnits.Y, RotationwUnits.toRawQuaternion(scene.nodes[nodeId].origin.orientation || EULER_IDENTITY)), Vector3wUnits.Y));
+const yAngle = (nodeId) => -180 / Math.PI * Math.asin(Vector3wUnits.dot(Vector3wUnits.applyQuaternion(Vector3wUnits.Y, RotationwUnits.toRawQuaternion(scene.nodes[nodeId].origin.orientation || EULER_IDENTITY)), Vector3wUnits.Y));
 
 
 scene.addOnRenderListener(() => {
-  // const currTime = Date.now();
-  // const timeDiff = currTime - startTime;
-  const upright2 = yAngle('can2') < 5;
-  const upright9 = yAngle('can9') < 5;
-  const upright10 = yAngle('can10') < 5;
-  // if(timeDiff > 1000) {
-  //   console.log('can6 angle: ', yAngle('can6'));
-  //   startTime = currTime;
-  // }
+  const upright2 = yAngle('can2') > 5;
   scene.setChallengeEventValue('can2Upright', upright2);
+
+});
+scene.addOnRenderListener(() => {
+  const upright9 = yAngle('can9') > 5;
   scene.setChallengeEventValue('can9Upright', upright9);
+
+});
+scene.addOnRenderListener(() => {
+  const upright10 = yAngle('can10') > 5;
   scene.setChallengeEventValue('can10Upright', upright10);
+
 });
 `;
 
+
+
+const ROBOT_ORIGIN: ReferenceFramewUnits = {
+  ...baseScene.nodes['robot'].origin,
+  position: {
+    ...baseScene.nodes['robot'].origin.position,
+
+  },
+};
+
 export const JBC_7: Scene = {
   ...baseScene,
-  name: { [LocalizedString.EN_US]: "JBC 7" },
+  name: { [LocalizedString.EN_US]: 'JBC 7' },
   description: {
     [LocalizedString.EN_US]: `Junior Botball Challenge 7: Load 'Em Up`,
   },
   scripts: {
-    uprightCans: Script.ecmaScript("Upright Cans", uprightCans),
-    garageIntersects: Script.ecmaScript("Garage Intersects", garageIntersects),
+    uprightCans: Script.ecmaScript('Upright Cans', uprightCans),
+    canIntersectsGarage: Script.ecmaScript('Can Intersects Garage', canIntersectsGarage),
+    leftStartBox: Script.ecmaScript('Left Start Box', leftStartBox),
   },
   geometry: {
     ...baseScene.geometry,
-    mainSurface_geom: {
+    circle_geom: {
+      type: 'cylinder',
+      radius: Distance.centimeters(3),
+      height: Distance.centimeters(0.1),
+    },
+    notStartBox_geom: {
       type: "box",
       size: {
         x: Distance.meters(3.54),
-        y: Distance.centimeters(0.1),
-        z: Distance.meters(3.54),
+        y: Distance.centimeters(10),
+        z: Distance.meters(2.13),
       },
     },
     startBox_geom: {
-      type: "box",
+      type: 'box',
       size: {
-        x: Distance.meters(3.54),
+        x: Distance.centimeters(59),
         y: Distance.centimeters(0.1),
-        z: Distance.centimeters(0),
+        z: Distance.centimeters(30),
       },
     },
     greenGarage_geom: {
-      type: "box",
+      type: 'box',
       size: {
-        x: Distance.centimeters(20),
+        x: Distance.centimeters(23),
         y: Distance.centimeters(0.1),
-        z: Distance.centimeters(20),
+        z: Distance.centimeters(23),
+      },
+    },
+    yellowGarage_geom: {
+      type: 'box',
+      size: {
+        x: Distance.centimeters(19),
+        y: Distance.centimeters(0.1),
+        z: Distance.centimeters(19),
+      },
+    },
+    blueGarage_geom: {
+      type: 'box',
+      size: {
+        x: Distance.centimeters(20.5),
+        y: Distance.centimeters(0.1),
+        z: Distance.centimeters(21),
       },
     },
 
-    blueGarage_geom: {
-      type: "box",
+
+    endBox_geom: {
+      type: 'box',
       size: {
-        x: Distance.centimeters(20),
-        y: Distance.centimeters(0.1),
-        z: Distance.centimeters(20),
+        x: Distance.inches(24),
+        y: Distance.centimeters(15),
+        z: Distance.centimeters(1),
       },
     },
+
   },
   nodes: {
     ...baseScene.nodes,
-    mainSurface: {
-      type: "object",
-      geometryId: "mainSurface_geom",
-      name: { [LocalizedString.EN_US]: "Mat Surface" },
-      visible: false,
-      origin: {
-        position: {
-          x: Distance.centimeters(0),
-          y: Distance.centimeters(-6.9),
-          z: Distance.inches(19.75),
-        },
-      },
-      material: {
-        type: "basic",
-        color: {
-          type: "color3",
-          color: Color.rgb(0, 0, 0),
-        },
-      },
+    robot: {
+      ...baseScene.nodes['robot'],
+      startingOrigin: ROBOT_ORIGIN,
+      origin: ROBOT_ORIGIN,
     },
     startBox: {
-      type: "object",
-      geometryId: "startBox_geom",
-      name: { [LocalizedString.EN_US]: "Start Box" },
+      type: 'object',
+      geometryId: 'startBox_geom',
+      name: { [LocalizedString.EN_US]: 'Start Box' },
       visible: false,
       origin: {
         position: {
           x: Distance.centimeters(0),
-          y: Distance.centimeters(-6.9),
-          z: Distance.centimeters(0),
+          y: Distance.centimeters(-6.89),
+          z: Distance.centimeters(0.6),
         },
       },
       material: {
-        type: "pbr",
-        emissive: {
-          type: "color3",
-          color: Color.rgb(255, 255, 255),
+        type: 'basic',
+        color: {
+          type: 'color3',
+          color: Color.rgb(0, 0, 255),
         },
       },
     },
-
     greenGarage: {
-      type: "object",
-      geometryId: "greenGarage_geom",
-      name: { [LocalizedString.EN_US]: "Green Garage" },
+      type: 'object',
+      geometryId: 'greenGarage_geom',
+      name: { [LocalizedString.EN_US]: 'Green Garage' },
       visible: false,
       origin: {
         position: {
-          x: Distance.centimeters(0),
-          y: Distance.centimeters(-6.9),
-          z: Distance.centimeters(53.2),
+          x: Distance.centimeters(0.1),
+          y: Distance.centimeters(-6.89),
+          z: Distance.centimeters(53),
         },
       },
       material: {
-        type: "pbr",
-        emissive: {
-          type: "color3",
-          color: Color.rgb(255, 255, 255),
+        type: 'basic',
+        color: {
+          type: 'color3',
+          color: Color.rgb(0, 255, 0),
+        },
+      },
+    },
+    yellowGarage: {
+      type: 'object',
+      geometryId: 'yellowGarage_geom',
+      name: { [LocalizedString.EN_US]: 'Yellow Garage' },
+      visible: false,
+      origin: {
+        position: {
+          x: Distance.centimeters(18.8),
+          y: Distance.centimeters(-6.89),
+          z: Distance.centimeters(78.2),
+        },
+      },
+      material: {
+        type: 'basic',
+        color: {
+          type: 'color3',
+          color: Color.rgb(255, 0, 0),
         },
       },
     },
 
     blueGarage: {
-      type: "object",
-      geometryId: "blueGarage_geom",
-      name: { [LocalizedString.EN_US]: "Blue Garage" },
+      type: 'object',
+      geometryId: 'blueGarage_geom',
+      name: { [LocalizedString.EN_US]: 'Blue Garage' },
+      visible: false,
+      origin: {
+        position: {
+          x: Distance.centimeters(-12.6),
+          y: Distance.centimeters(-6.89),
+          z: Distance.centimeters(94.1),
+        },
+        orientation: {
+          type: 'euler',
+          x: Angle.degrees(0),
+          y: Angle.degrees(45),
+          z: Angle.degrees(0),
+        },
+      },
+      material: {
+        type: 'basic',
+        color: {
+          type: 'color3',
+          color: Color.rgb(0, 0, 255),
+        },
+      },
+      editable: true,
+
+    },
+
+    endBox: {
+      type: 'object',
+      geometryId: 'endBox_geom',
+      name: { [LocalizedString.EN_US]: 'End Box' },
       visible: false,
       origin: {
         position: {
           x: Distance.centimeters(0),
-          y: Distance.centimeters(-6.9),
-          z: Distance.centimeters(53.2),
+          y: Distance.centimeters(0),
+          z: Distance.inches(43.685), // 24 for half the mat and 19.685 for the mat's origin
         },
       },
       material: {
-        type: "pbr",
+        type: 'pbr',
         emissive: {
-          type: "color3",
-          color: Color.rgb(255, 255, 255),
+          type: 'color3',
+          color: Color.rgb(0, 255, 0),
         },
       },
     },
-    ...baseScene.nodes,
-    can2: createCanNode(2),
-    can9: createCanNode(9),
-    can10: createCanNode(10),
+
+    circle2: {
+      type: 'object',
+      geometryId: 'circle_geom',
+      name: { [LocalizedString.EN_US]: 'Circle 2' },
+      visible: false,
+      origin: {
+        position: {
+          ...canPositions[1],
+          y: Distance.centimeters(-6.9),
+        },
+      },
+      material: {
+        type: 'basic',
+        color: {
+          type: 'color3',
+          color: Color.rgb(25, 29, 95),
+        },
+
+      },
+    },
+
+    circle9: {
+      type: 'object',
+      geometryId: 'circle_geom',
+      name: { [LocalizedString.EN_US]: 'Circle 9' },
+      visible: false,
+      origin: {
+        position: {
+          ...canPositions[8],
+          y: Distance.centimeters(-6.9),
+        },
+      },
+      material: {
+        type: 'basic',
+        color: {
+          type: 'color3',
+          color: Color.rgb(25, 29, 95),
+        },
+
+      },
+    },
+
+    circle10: {
+      type: 'object',
+      geometryId: 'circle_geom',
+      name: { [LocalizedString.EN_US]: 'Circle 10' },
+      visible: false,
+      origin: {
+        position: {
+          ...canPositions[9],
+          y: Distance.centimeters(-6.9),
+        },
+      },
+      material: {
+        type: 'basic',
+        color: {
+          type: 'color3',
+          color: Color.rgb(25, 29, 95),
+        },
+
+      },
+    },
+
+
+    can2: {
+      ...createCanNode(2, { x: Distance.centimeters(0), y: Distance.centimeters(0), z: Distance.centimeters(28.8) }),
+    },
+
+    can9: {
+      ...createCanNode(9, { x: Distance.centimeters(0), y: Distance.centimeters(0), z: Distance.centimeters(85.4) }, true),
+    },
+    can10: {
+      ...createCanNode(10, { x: Distance.centimeters(19.3), y: Distance.centimeters(0), z: Distance.centimeters(96.9) }, false),
+    }
   },
 };
-
