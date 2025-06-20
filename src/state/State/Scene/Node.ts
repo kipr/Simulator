aadd972885a0f1d2ddead670ec7234d1f02022bc
twells46 <173561638+twells46@users.noreply.github.com>
2025-06-20 11:26:55 -0500

    Node can be added

--- src/state/State/Scene/Node.ts
+++ src/state/State/Scene/Node.ts
@@ -602,6 +602,7 @@ namespace Node {
       case 'from-jbc-template': return FromJBCTemplate.from(node);
       case 'from-rock-template': return FromRockTemplate.from(node);
       case 'from-space-template': return FromSpaceTemplate.from(node);
+      case 'from-bb-template': return FromBBTemplate.from(node);
       case 'robot': return Robot.from(node);
     }
   };
