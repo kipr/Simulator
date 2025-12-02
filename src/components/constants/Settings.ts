56b3caff3936c7d6b9309c3c0a720b34ae30562d
Erin <erinharrington12@yahoo.com>
2025-12-02 12:34:13 -0600

    Added extra Settings field to help with new IDE settings (Voldigate)

--- src/components/constants/Settings.ts
+++ src/components/constants/Settings.ts
@@ -2,10 +2,15 @@ export interface Settings {
   simulationSensorNoise: boolean;
   simulationRealisticSensors: boolean;
   editorAutoComplete: boolean;
+  ideEditorDarkMode: boolean;
+  classroomView: boolean;
+  consoleLayout: "horizontal" | "vertical";
 }
-
 export const DEFAULT_SETTINGS: Settings = {
   simulationSensorNoise: false,
   simulationRealisticSensors: false,
   editorAutoComplete: false,
+  ideEditorDarkMode: false,
+  classroomView: false,
+  consoleLayout: "horizontal",
 };
