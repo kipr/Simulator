6db5cfe4b04553afc6020eb86725362726fa1e5d
Erin <erinharrington12@yahoo.com>
2025-12-05 12:50:29 -0600

    Temporary challenge badge visuals

---
+++ src/components/constants/challenge-badges.ts
@@ -0,0 +1,54 @@
+import CHALLENGE_0 from '../../../static/assets/challenge-0.png';
+import CHALLENGE_1 from '../../../static/assets/challenge-1.png';
+import CHALLENGE_2 from '../../../static/assets/challenge-2.png';
+import CHALLENGE_3 from '../../../static/assets/challenge-3.png';
+import CHALLENGE_4 from '../../../static/assets/challenge-4.png';
+import CHALLENGE_5 from '../../../static/assets/challenge-5.png';
+import CHALLENGE_6 from '../../../static/assets/challenge-6.png';
+import CHALLENGE_7 from '../../../static/assets/challenge-7.png';
+import CHALLENGE_8 from '../../../static/assets/challenge-8.png';
+import CHALLENGE_9 from '../../../static/assets/challenge-9.png';
+import CHALLENGE_10 from '../../../static/assets/challenge-10.png';
+import CHALLENGE_11 from '../../../static/assets/challenge-11.png';
+import CHALLENGE_12 from '../../../static/assets/challenge-12.png';
+import CHALLENGE_13 from '../../../static/assets/challenge-13.png';
+import CHALLENGE_14 from '../../../static/assets/challenge-14.png';
+import CHALLENGE_15 from '../../../static/assets/challenge-15.png';
+import CHALLENGE_16 from '../../../static/assets/challenge-16.png';
+import CHALLENGE_17 from '../../../static/assets/challenge-17.png';
+import CHALLENGE_18 from '../../../static/assets/challenge-18.png';
+import CHALLENGE_19 from '../../../static/assets/challenge-19.png';
+import CHALLENGE_20 from '../../../static/assets/challenge-20.png';
+import CHALLENGE_21 from '../../../static/assets/challenge-21.png';
+import CHALLENGE_22 from '../../../static/assets/challenge-22.png';
+import CHALLENGE_23 from '../../../static/assets/challenge-23.png';
+import CHALLENGE_24 from '../../../static/assets/challenge-24.png';
+import DOTTED from '../../../static/assets/challenge-24-dotted.png';
+export const CHALLENGE_BADGES = [
+  CHALLENGE_0,
+  CHALLENGE_1,
+  CHALLENGE_2,
+  CHALLENGE_3,
+  CHALLENGE_4,
+  CHALLENGE_5,
+  CHALLENGE_6,
+  CHALLENGE_7,
+  CHALLENGE_8,
+  CHALLENGE_9,
+  CHALLENGE_10,
+  CHALLENGE_11,
+  CHALLENGE_12,
+  CHALLENGE_13,
+  CHALLENGE_14,
+  CHALLENGE_15,
+  CHALLENGE_16,
+  CHALLENGE_17,
+  CHALLENGE_18,
+  CHALLENGE_19,
+  CHALLENGE_20,
+  CHALLENGE_21,
+  CHALLENGE_22,
+  CHALLENGE_23,
+  CHALLENGE_24,
+  DOTTED
+]
