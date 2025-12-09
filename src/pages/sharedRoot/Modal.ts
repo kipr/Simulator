11e866f0d857765590b3eec1913983a6980746ef
Erin <erinharrington12@yahoo.com>
2025-12-09 16:32:16 -0600

    Added "New Project" Modal

--- src/pages/sharedRoot/Modal.ts
+++ src/pages/sharedRoot/Modal.ts
@@ -16,86 +16,93 @@ export namespace Modal {
     CopyScene,
     SettingsScene,
     DeleteRecord,
-    ResetCode
+    ResetCode,
+    NewProject
   }
-  
+
   export interface Settings {
     type: Type.Settings;
   }
-  
+
   export const SETTINGS: Settings = { type: Type.Settings };
-  
+
   export interface About {
     type: Type.About;
   }
-  
+
   export const ABOUT: About = { type: Type.About };
-  
+
   export interface Feedback {
     type: Type.Feedback;
   }
-  
+
   export const FEEDBACK: Feedback = { type: Type.Feedback };
-  
+
   export interface FeedbackSuccess {
     type: Type.FeedbackSuccess;
   }
-  
+
   export const FEEDBACKSUCCESS: FeedbackSuccess = { type: Type.FeedbackSuccess };
-  
+
   export interface Exception {
     type: Type.Exception;
     error: Error;
     info?: React.ErrorInfo;
   }
-  
+
   export const exception = (error: Error, info?: React.ErrorInfo): Exception => ({ type: Type.Exception, error, info });
-  
+
   export interface SelectScene {
     type: Type.OpenScene;
   }
-  
+
   export const SELECT_SCENE: SelectScene = { type: Type.OpenScene };
-  
+
   export interface None {
     type: Type.None;
   }
-  
+
   export const NONE: None = { type: Type.None };
-  
+
   export interface NewScene {
     type: Type.NewScene;
   }
-  
+
   export const NEW_SCENE: NewScene = { type: Type.NewScene };
-  
+
   export interface CopyScene {
     type: Type.CopyScene;
     scene: Scene;
   }
-  
+
   export const copyScene = construct<CopyScene>(Type.CopyScene);
-  
+
   export interface DeleteRecord {
     type: Type.DeleteRecord;
     record: Record;
   }
-  
+
   export const deleteRecord = construct<DeleteRecord>(Type.DeleteRecord);
-  
+
   export interface SettingsScene {
     type: Type.SettingsScene;
   }
-  
+
   export const SETTINGS_SCENE: SettingsScene = { type: Type.SettingsScene };
-  
+
   export interface ResetCode {
     type: Type.ResetCode;
   }
-  
+
   export const RESET_CODE: ResetCode = { type: Type.ResetCode };
+
+  export interface NewProject {
+    type: Type.NewProject;
+  }
+
+  export const NEW_PROJECT: NewProject = { type: Type.NewProject };
 }
-  
+
 export type Modal = (
   Modal.Settings |
   Modal.About |
@@ -108,5 +115,6 @@ export type Modal = (
   Modal.CopyScene |
   Modal.DeleteRecord |
   Modal.SettingsScene |
-  Modal.ResetCode
+  Modal.ResetCode |
+  Modal.NewProject
 );
