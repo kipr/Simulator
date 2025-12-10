e4fcc303466a3e445ab588478b5c0640b314c711
Erin <erinharrington12@yahoo.com>
2025-12-10 16:25:22 -0600

    Added default code for graphical language

--- src/programming/compiler/ProgrammingLanguage.ts
+++ src/programming/compiler/ProgrammingLanguage.ts
@@ -13,7 +13,20 @@ namespace ProgrammingLanguage {
     c: '#include <stdio.h>\n#include <kipr/wombat.h>\n\nint main()\n{\n  printf("Hello, World!\\n");\n\n  return 0;\n}\n',
     cpp: '#include <iostream>\n#include <kipr/wombat.hpp>\n\nint main()\n{\n  std::cout << "Hello, World!" << std::endl;\n\n  return 0;\n}\n',
     python: 'from kipr import *\n\nprint(\'Hello, World!\')',
-    graphical: ''
+    graphical: `<xml xmlns="http://www.w3.org/1999/xhtml">
+                <variables></variables>
+                <block type="control_run" id="Tr7;}P}KM[{|.$;Wo9_1" x="176" y="129">
+                  <next>
+                    <block type="control_printf" id="*wt6^HnMX#w},]R6)*w}">
+                      <value name="STRING">
+                        <shadow type="text" id="2R).HZf7nqV:od^Se=3+">
+                          <field name="TEXT">Hello, World!</field>
+                        </shadow>
+                      </value>
+                    </block>
+                  </next>
+                </block>
+              </xml>`
   };
 }
 
