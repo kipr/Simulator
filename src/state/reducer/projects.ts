77d778305e3b579dbdd2730d223c89f0e328c3ef
Erin <erinharrington12@yahoo.com>
2025-12-10 16:25:52 -0600

    Added project actions add file, set code, select project, and delete project

--- src/state/reducer/projects.ts
+++ src/state/reducer/projects.ts
@@ -5,7 +5,7 @@ import db from '../../db';
 import Selector from '../../db/Selector';
 import Dict from "../../util/objectOps/Dict";
 import store from "..";
-
+import ProgrammingLanguage from "../../programming/compiler/ProgrammingLanguage";
 
 export namespace ProjectsAction {
   export const addProject = construct<AddProject>("projects/add-project");
@@ -27,11 +27,48 @@ export namespace ProjectsAction {
   export interface LoadProjects {
     type: "projects/load-projects";
   }
+
+  export const setCode = construct<SetCode>("projects/set-code");
+
+  export interface SetCode {
+    type: "projects/set-code";
+    project: Project;
+    fileName: string;
+    fileType: 'src' | 'include' | 'userData';
+    fileContent: string;
+  }
+
+  export const addFile = construct<AddFile>("projects/add-file");
+
+  export interface AddFile {
+    type: "projects/add-file";
+    project: Project;
+    fileName: string;
+    fileType: 'src' | 'include' | 'userData';
+  }
+
+  export const selectProject = construct<SelectProject>("projects/select-project");
+
+  export interface SelectProject {
+    type: "projects/select-project";
+    project: Project;
+  }
+
+  export const deleteProject = construct<DeleteProject>("projects/delete-project");
+
+  export interface DeleteProject {
+    type: "projects/delete-project";
+    project: Project;
+  }
 }
 
 export type ProjectsAction =
   | ProjectsAction.LoadProjects
   | ProjectsAction.AddProject
+  | ProjectsAction.AddFile
+  | ProjectsAction.SetCode
+  | ProjectsAction.SelectProject
+  | ProjectsAction.DeleteProject
   | ProjectsAction.SetProjects;
 
 
@@ -47,6 +84,7 @@ const load = async () => {
         value: projectData as Project
       });
     });
+    console.log("Dispatching loaded projects:", asyncProjects);
     store.dispatch(ProjectsAction.setProjects({
       projects: asyncProjects
     }));
@@ -56,7 +94,7 @@ const load = async () => {
   }
 }
 
-const create = async (projectName: string, next: Async.Creating<Project>) => {
+const createProject = async (projectName: string, next: Async.Creating<Project>) => {
   try {
     await db.set(Selector.project(projectName), next.value);
 
@@ -78,6 +116,78 @@ const create = async (projectName: string, next: Async.Creating<Project>) => {
 }
 
 
+const createFile = async (project: Project, fileName: string, fileType: 'src' | 'include' | 'userData') => {
+
+  try {
+    const updatedProject: Project = {
+      ...project,
+      [`${fileType}Files`]: {
+        ...project[`${fileType}Files`],
+        [fileName]: {
+          fileName,
+          fileContent: `${ProgrammingLanguage.DEFAULT_CODE[project.projectLanguage]}`
+        }
+      }
+    };
+    await db.set(Selector.project(project.projectName), updatedProject);
+    const asyncProject: AsyncProject = Async.loaded({
+      brief: {},
+      value: updatedProject
+    });
+    console.log("Dispatching new file project update:", asyncProject);
+    store.dispatch(ProjectsAction.setProjects({
+      projects: {
+        [project.projectName]: asyncProject
+      }
+    }));
+  }
+  catch (error) {
+    console.log("Error creating new file:", error);
+  }
+}
+
+export const deleteProject = async (project: Project) => {
+  try {
+    await db.delete(Selector.project(project.projectName));
+
+    store.dispatch(ProjectsAction.deleteProject({
+      project
+    }));
+  }
+  catch (error) {
+    console.log("Error deleting project:", error);
+  }
+}
+const save = async (project: Project, fileName: string, fileType: 'src' | 'include' | 'userData', fileContent: string) => {
+  try {
+    const updatedProject: Project = {
+      ...project,
+      [`${fileType}Files`]: {
+        ...project[`${fileType}Files`],
+        [fileName]: {
+          fileName,
+          fileContent: fileContent
+        }
+      }
+    };
+    await db.set(Selector.project(project.projectName), updatedProject);
+    const asyncProject: AsyncProject = Async.loaded({
+      brief: {},
+      value: updatedProject
+    });
+
+    store.dispatch(ProjectsAction.setProjects({
+      projects: {
+        [project.projectName]: asyncProject
+      }
+    }));
+  }
+  catch (error) {
+    console.log("Error saving project code:", error);
+  }
+}
+
+
 
 export const reduceProjects = (
   state: ProjectsState = { entities: {}, selectedProject: null },
@@ -89,15 +199,38 @@ export const reduceProjects = (
       void load();
       return state;
     }
+    case "projects/set-code": {
+      console.log("projects/set-code");
+      void save(action.project, action.fileName, action.fileType, action.fileContent);
+      return state;
+
+    }
     case "projects/add-project": {
       console.log("Adding project", action.project.projectName, action.project);
       const creating = Async.creating({ value: action.project });
-      void create(action.project.projectName, creating);
+      void createProject(action.project.projectName, creating);
+      return state;
+    }
+    case "projects/add-file": {
+      void createFile(action.project, action.fileName, action.fileType);
       return state;
     }
     case "projects/set-projects": {
       const merged = { ...state.entities, ...action.projects };
       return { ...state, entities: merged };
+    }
+    case "projects/select-project": {
+      if (state.selectedProject && state.selectedProject.projectName === action.project.projectName) {
+        return { ...state, selectedProject: null };
+      }
+      return { ...state, selectedProject: action.project };
+    }
+    case "projects/delete-project": {
+      const entities = { ...state.entities };
+      delete entities[action.project.projectName];
+      if (state.selectedProject && state.selectedProject.projectName === action.project.projectName) {
+        return { ...state, entities, selectedProject: null };
+      }
     }
     default:
       return state;
