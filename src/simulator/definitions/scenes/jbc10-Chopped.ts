590144c4f752b06424ad099251c5a34fc78b48ea
twells46 <173561638+twells46@users.noreply.github.com>
2026-01-06 11:00:12 -0600

    Update JBC 10 to target arm

--- src/simulator/definitions/scenes/jbc10-Chopped.ts
+++ src/simulator/definitions/scenes/jbc10-Chopped.ts
@@ -1,29 +1,40 @@
 import Scene from '../../../state/State/Scene';
-import LocalizedString from '../../../util/LocalizedString';
 import { createBaseSceneSurfaceA, createCanNode } from './jbcBase';
-import { Color } from '../../../state/State/Scene/Color';
-import { Distance } from '../../../util';
 import Script from '../../../state/State/Scene/Script';
+import { matAStartGeoms, matAStartNodes, notInStartBox } from './jbcCommonComponents';
 import tr from '@i18n';
 
 const baseScene = createBaseSceneSurfaceA();
 
-const notInStartBox = `
-scene.addOnIntersectionListener('robot', (type, otherNodeId) => {
-  // console.log('Robot not started in start box!', type, otherNodeId);
-  if(scene.programStatus === 'running'){
-    scene.setChallengeEventValue('notInStartBox', type === 'start');
-  }
-}, 'notStartBox');
-`;
-
 const waitToChop = `
-// Need to wait 5 seconds before the can can be chopped
+let can7Chopped = false;
+scene.addOnIntersectionListener('claw_link', (type, otherNodeId) => {
+  if(scene.programStatus === 'running'){
+    // console.log('can7 touched');
+    can7Chopped = true;
+    scene.setChallengeEventValue('can7Chopped', can7Chopped);
+  }
+}, 'can7');
+scene.addOnIntersectionListener('arm_link', (type, otherNodeId) => {
+  if(scene.programStatus === 'running'){
+    // console.log('can7 touched');
+    can7Chopped = true;
+    scene.setChallengeEventValue('can7Chopped', can7Chopped);
+  }
+}, 'can7');
+
+// Need to wait 3 seconds before the can can be chopped
 let currentDate;
 let stopTime = 0;
 scene.addOnRenderListener(() => {
   const robotNode = scene.nodes['robot'];
-  if (robotNode.state.getMotor(0).speedGoal <= 15 && robotNode.state.getMotor(3).speedGoal <= 15) {
+
+  if (
+  robotNode.state.motors[0].speedGoal <= 15 &&
+  robotNode.state.motors[3].speedGoal <= 15 &&
+  !can7Chopped &&
+  scene.programStatus === 'running'
+  ) {
     if (stopTime == 0) {
       currentDate = new Date();
       stopTime = currentDate.getTime();
@@ -31,27 +42,15 @@ scene.addOnRenderListener(() => {
     }
     else {
       currentDate = new Date();
-      if (currentDate.getTime() - stopTime > 5000) {
+      if (currentDate.getTime() - stopTime > 3000) {
         scene.setChallengeEventValue('waitedToChop', true);
-        // console.log('Waited 5 seconds');
+        // console.log('Waited 3 seconds');
       }
     }
   }
   else {
     stopTime = 0;
   }
-});
-`;
-
-const uprightCan = `
-// When a can is standing upright, the upright condition is met.
-
-const EULER_IDENTITY = RotationwUnits.EulerwUnits.identity();
-const yAngle = (nodeId) => 180 / Math.PI * -1 * Math.asin(Vector3wUnits.dot(Vector3wUnits.applyQuaternion(Vector3wUnits.Y, RotationwUnits.toRawQuaternion(scene.nodes[nodeId].origin.orientation || EULER_IDENTITY)), Vector3wUnits.Y));
-
-scene.addOnRenderListener(() => {
-  const upright7 = yAngle('can7') > 5;
-  scene.setChallengeEventValue('can7Upright', upright7);
 });
 `;
 
@@ -62,41 +61,14 @@ export const JBC_10: Scene = {
   scripts: {
     inStartBox: Script.ecmaScript('In Start Box', notInStartBox),
     waitToChop: Script.ecmaScript('Wait to Chop', waitToChop),
-    uprightCan: Script.ecmaScript('Upright Can', uprightCan),
   },
   geometry: {
     ...baseScene.geometry,
-    notStartBox_geom: {
-      type: 'box',
-      size: {
-        x: Distance.meters(3.54),
-        y: Distance.centimeters(10),
-        z: Distance.meters(2.13),
-      },
-    },
+    ...matAStartGeoms,
   },
   nodes: {
     ...baseScene.nodes,
-    notStartBox: {
-      type: 'object',
-      geometryId: 'notStartBox_geom',
-      name: tr('Not Start Box'),
-      visible: false,
-      origin: {
-        position: {
-          x: Distance.centimeters(0),
-          y: Distance.centimeters(-1.9),
-          z: Distance.meters(1.208),
-        },
-      },
-      material: {
-        type: 'basic',
-        color: {
-          type: 'color3',
-          color: Color.rgb(255, 0, 0),
-        },
-      },
-    },
+    ...matAStartNodes,
     can7: createCanNode(7),
   },
 };
