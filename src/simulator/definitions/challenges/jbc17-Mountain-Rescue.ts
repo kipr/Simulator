8e7c0690df5168a1affe121c79d7c3dd1c621ca2
twells46 <173561638+twells46@users.noreply.github.com>
2026-01-13 13:16:02 -0600

    JBC 17

--- src/simulator/definitions/challenges/jbc17-Mountain-Rescue.ts
+++ src/simulator/definitions/challenges/jbc17-Mountain-Rescue.ts
@@ -22,29 +22,17 @@ export default {
       name: tr('Robot not in Start Box'),
       description: tr('Robot not in start box'),
     },
-    can1Upright: {
-      name: tr('Can 1 Upright'),
-      description: tr('Can 1 is upright'),
+    can1Retrieved: {
+      name: tr('Can 1 Rescued'),
+      description: tr('Can 1 rescued'),
     },
-    can2Upright: {
-      name: tr('Can 2 Upright'),
-      description: tr('Can 2 is upright'),
+    can2Retrieved: {
+      name: tr('Can 2 Rescued'),
+      description: tr('Can 2 rescued'),
     },
-    can3Upright: {
-      name: tr('Can 3 Upright'),
-      description: tr('Can 3 is upright'),
-    },
-    can1Intersects: {
-      name: tr('Can 1 Intersects'),
-      description: tr('Can 1 rescued intersecting starting box'),
-    },
-    can2Intersects: {
-      name: tr('Can 2 Intersects'),
-      description: tr('Can 2 rescued intersecting starting box'),
-    },
-    can3Intersects: {
-      name: tr('Can 3 Intersects'),
-      description: tr('Can 3 rescued intersecting starting box'),
+    can3Retrieved: {
+      name: tr('Can 3 Rescued'),
+      description: tr('Can 3 rescued'),
     },
   },
   success: {
@@ -63,89 +51,31 @@ export default {
         argId: "inStartBox",
       },
 
-      // Rescued upright can events
-      can1Upright: {
+      can1Retrieved: {
         type: Expr.Type.Event,
-        eventId: 'can1Upright',
+        eventId: "can1Retrieved",
       },
-      can1NotUpright: {
-        type: Expr.Type.Not,
-        argId: 'can1Upright',
-      },
-      can2Upright: {
+      can2Retrieved: {
         type: Expr.Type.Event,
-        eventId: 'can2Upright',
+        eventId: "can2Retrieved",
       },
-      can2NotUpright: {
-        type: Expr.Type.Not,
-        argId: 'can2Upright',
-      },
-      can3Upright: {
+      can3Retrieved: {
         type: Expr.Type.Event,
-        eventId: 'can3Upright',
+        eventId: "can3Retrieved",
       },
-      can3NotUpright: {
-        type: Expr.Type.Not,
-        argId: 'can3Upright',
-      },
-
-      // Rescued intersecting can events
-      can1Intersects: {
-        type: Expr.Type.Event,
-        eventId: 'can1Intersects',
-      },
-      can2Intersects: {
-        type: Expr.Type.Event,
-        eventId: 'can2Intersects',
-      },
-      can3Intersects: {
-        type: Expr.Type.Event,
-        eventId: 'can3Intersects',
-      },
-
-      // Intersecting and Upright
-      intersectingUpright1: {
-        type: Expr.Type.And,
-        argIds: ['can1Upright', 'can1Intersects'],
-      },
-      intersectingUpright1Once: {
-        type: Expr.Type.Once,
-        argId: 'intersectingUpright1',
-      },
-      intersectingUpright2: {
-        type: Expr.Type.And,
-        argIds: ['can2Upright', 'can2Intersects'],
-      },
-      intersectingUpright2Once: {
-        type: Expr.Type.Once,
-        argId: 'intersectingUpright2',
-      },
-      intersectingUpright3: {
-        type: Expr.Type.And,
-        argIds: ['can3Upright', 'can3Intersects'],
-      },
-      intersectingUpright3Once: {
-        type: Expr.Type.Once,
-        argId: 'intersectingUpright3',
-      },
-
       completion: {
         type: Expr.Type.And,
-        argIds: ['inStartBoxOnce', 'intersectingUpright1Once', 'intersectingUpright2Once', 'intersectingUpright3Once'],
+        argIds: ['inStartBoxOnce', 'can1Retrieved', 'can2Retrieved', 'can3Retrieved'],
       },
     },
     rootId: 'completion',
   },
   successGoals: [
     { exprId: 'inStartBoxOnce', name: tr('Start in the Start Box') },
-    { exprId: 'can1Intersects', name: tr('Rescue can 1 to start box') },
-    { exprId: 'can2Intersects', name: tr('Rescue can 2 to start box') },
-    { exprId: 'can3Intersects', name: tr('Rescue can 3 to start box') },
+    { exprId: 'can1Retrieved', name: tr('Rescue can 1 to start box') },
+    { exprId: 'can2Retrieved', name: tr('Rescue can 2 to start box') },
+    { exprId: 'can3Retrieved', name: tr('Rescue can 3 to start box') },
   ],
-  failureGoals: [
-    { exprId: 'can1NotUpright', name: tr('Can 1 not upright') },
-    { exprId: 'can2NotUpright', name: tr('Can 2 not upright') },
-    { exprId: 'can3NotUpright', name: tr('Can 3 not upright') },
-  ],
+  failureGoals: [],
   sceneId: 'jbc17',
 } as Challenge;
