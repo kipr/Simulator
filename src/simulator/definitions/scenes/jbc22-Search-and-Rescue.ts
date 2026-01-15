b2c15594edcc9165905a0dc3bd099b22616e68e6
twells46 <173561638+twells46@users.noreply.github.com>
2026-01-14 23:15:19 -0600

    JBC 22

--- src/simulator/definitions/scenes/jbc22-Search-and-Rescue.ts
+++ src/simulator/definitions/scenes/jbc22-Search-and-Rescue.ts
@@ -1,10 +1,8 @@
 import Scene from '../../../state/State/Scene';
-import { Distance } from '../../../util';
 import Script from '../../../state/State/Scene/Script';
 import { createCanNode, createBaseSceneSurfaceA } from './jbcBase';
-import { Color } from '../../../state/State/Scene/Color';
 import tr from '@i18n';
-import { matAStartGeoms, matAStartNodes, setNodeVisible, notInStartBox } from './jbcCommonComponents';
+import { matAStartGeoms, matAStartNodes, setNodeVisible, notInStartBox, nodeUpright } from './jbcCommonComponents';
 
 const baseScene = createBaseSceneSurfaceA();
 
@@ -21,13 +19,9 @@ scene.addOnIntersectionListener('can', (type, otherNodeId) => {
 `;
 
 const uprightCan = `
-// When a can is standing upright, the upright condition is met.
-const EULER_IDENTITY = RotationwUnits.EulerwUnits.identity();
-const yAngle = (nodeId) => 180 / Math.PI * -1 * Math.asin(Vector3wUnits.dot(Vector3wUnits.applyQuaternion(Vector3wUnits.Y, RotationwUnits.toRawQuaternion(scene.nodes[nodeId].origin.orientation || EULER_IDENTITY)), Vector3wUnits.Y));
-
+${nodeUpright}
 scene.addOnRenderListener(() => {
-  const upright = yAngle('can') > 5;
-  scene.setChallengeEventValue('canUpright', upright);
+  scene.setChallengeEventValue('canUpright', nodeUpright('can'));
 });
 `;
 
@@ -49,38 +43,10 @@ export const JBC_22: Scene = {
   geometry: {
     ...baseScene.geometry,
     ...matAStartGeoms,
-    startBox_geom: {
-      type: 'box',
-      size: {
-        x: Distance.meters(3.54),
-        y: Distance.centimeters(0.1),
-        z: Distance.centimeters(30),
-      },
-    },
   },
   nodes: {
     ...baseScene.nodes,
     ...matAStartNodes,
-    startBox: {
-      type: 'object',
-      geometryId: 'startBox_geom',
-      name: tr('Start Box'),
-      visible: false,
-      origin: {
-        position: {
-          x: Distance.centimeters(0),
-          y: Distance.centimeters(-6.9),
-          z: Distance.centimeters(-3),
-        },
-      },
-      material: {
-        type: 'pbr',
-        emissive: {
-          type: 'color3',
-          color: Color.rgb(255, 255, 255),
-        },
-      },
-    },
     can: createCanNode(randomCan()),
   },
 };
