830d022252d3118587a7c7fbf4ee323e1a872dda
twells46 <173561638+twells46@users.noreply.github.com>
2026-01-20 10:35:41 -0600

    Add more sample limited challenges

--- src/simulator/definitions/limitedChallenges/index.ts
+++ src/simulator/definitions/limitedChallenges/index.ts
@@ -3,14 +3,24 @@ import { LimitedChallengeBrief } from '../../../state/State/LimitedChallenge';
 import Async from '../../../state/State/Async';
 
 import speedRunRingAroundTheCan from './speed-run-ring-around-the-can';
+import speedRunTagYoureIt from './speed-run-tag-youre-it';
+import speedRunDriveStraight from './speed-run-drive-straight';
 
 /**
  * Pre-built limited challenges ready for use in the reducer's default state.
  * Add new limited challenges here by importing them and adding an entry to this object.
  */
 export const DEFAULT_LIMITED_CHALLENGES: LimitedChallenges = {
+  'limited-speed-run-3': Async.loaded({
+    value: speedRunDriveStraight,
+    brief: LimitedChallengeBrief.fromChallenge(speedRunDriveStraight),
+  }),
   'limited-speed-run-1': Async.loaded({
     value: speedRunRingAroundTheCan,
     brief: LimitedChallengeBrief.fromChallenge(speedRunRingAroundTheCan),
+  }),
+  'limited-speed-run-2': Async.loaded({
+    value: speedRunTagYoureIt,
+    brief: LimitedChallengeBrief.fromChallenge(speedRunTagYoureIt),
   }),
 };
---
+++ src/simulator/definitions/limitedChallenges/speed-run-tag-youre-it.ts
@@ -0,0 +1,137 @@
+import Author from "../../../db/Author";
+import Expr from "../../../state/State/Challenge/Expr";
+import ProgrammingLanguage from "../../../programming/compiler/ProgrammingLanguage";
+import tr from '@i18n';
+import LimitedChallenge from "../../../state/State/LimitedChallenge";
+
+export default {
+  name: tr('Speed Run: Tag, You\'re it!'),
+  description: tr('Complete the Tag, You\'re it! challenge as fast as possible! Limited time event.'),
+  author: {
+    type: Author.Type.Organization,
+    id: 'kipr'
+  },
+  code: {
+    'c': ProgrammingLanguage.DEFAULT_CODE.c,
+    'cpp': ProgrammingLanguage.DEFAULT_CODE.cpp,
+    'python': ProgrammingLanguage.DEFAULT_CODE.python,
+  },
+  defaultLanguage: 'c',
+  events: {
+    notInStartBox: {
+      name: tr('Robot not in Start Box'),
+      description: tr('Robot not in start box'),
+    },
+    can9Touched: {
+      name: tr('Can 9 Touched'),
+      description: tr('Can A touched'),
+    },
+    can9Intersects: {
+      name: tr('Can 9 Intersects'),
+      description: tr('Can 9 intersects circle 9'),
+    },
+    can9Upright: {
+      name: tr('Can 9 Upright'),
+      description: tr('Can 9 upright on circle 9'),
+    },
+    returnStartBox: {
+      name: tr('Robot Rentered Start'),
+      description: tr('Robot reentered starting box'),
+    },
+  },
+  success: {
+    exprs: {
+      // Touch Events
+      can9Touched: {
+        type: Expr.Type.Event,
+        eventId: 'can9Touched',
+      },
+
+      // Start Box Events
+      notInStartBox: {
+        type: Expr.Type.Event,
+        eventId: "notInStartBox",
+      },
+      inStartBox: {
+        type: Expr.Type.Not,
+        argId: "notInStartBox",
+      },
+      inStartBoxOnce: {
+        type: Expr.Type.Once,
+        argId: "inStartBox",
+      },
+
+      returnStartBox: {
+        type: Expr.Type.Event,
+        eventId: 'returnStartBox',
+      },
+      returnStartBoxOnce: {
+        type: Expr.Type.Once,
+        argId: 'returnStartBox',
+      },
+
+      // Success Logic = Can A upright, intersects and touched
+      completion: {
+        type: Expr.Type.And,
+        argIds: ['can9Touched', 'inStartBoxOnce', 'returnStartBoxOnce'],
+      },
+    },
+    rootId: 'completion',
+  },
+  failure: {
+    exprs: {
+      // Intersects Events
+      can9Intersects: {
+        type: Expr.Type.Event,
+        eventId: 'can9Intersects',
+      },
+      can9NotIntersects: {
+        type: Expr.Type.Not,
+        argId: 'can9Intersects',
+      },
+
+      // Upright Events
+      can9Upright: {
+        type: Expr.Type.Event,
+        eventId: 'can9Upright',
+      },
+      can9NotUpright: {
+        type: Expr.Type.Not,
+        argId: 'can9Upright',
+      },
+
+      failure: {
+        type: Expr.Type.Or,
+        argIds: ['can9NotIntersects', 'can9NotUpright'],
+      },
+    },
+    rootId: 'failure',
+  },
+  successGoals: [
+    {
+      exprId: 'inStartBoxOnce',
+      name: tr('Start in the Start Box'),
+    },
+    {
+      exprId: 'can9Touched',
+      name: tr('Touch Can 9'),
+    },
+    {
+      exprId: 'returnStartBoxOnce',
+      name: tr('Return to the Start Box'),
+    },
+  ],
+  failureGoals: [
+    {
+      exprId: 'can9NotIntersects',
+      name: tr('Can 9 not in circle 9'),
+    },
+    {
+      exprId: 'can9NotUpright',
+      name: tr('Can 9 not upright'),
+    },
+  ],
+  sceneId: 'jbc1',
+  openDate: '2026-01-19T00:00:00Z',
+  closeDate: '2026-01-22T23:59:59Z',
+} as LimitedChallenge;
