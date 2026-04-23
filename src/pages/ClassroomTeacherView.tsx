4082f3216b0124800f89f4b6756ece31dba02b83
Erin <erinharrington12@yahoo.com>
2026-04-23 14:06:13 -0500

    Redoing UI for teacher view to allow teachers to see more details about created classroom and assign challenges to students

--- src/pages/ClassroomTeacherView.tsx
+++ src/pages/ClassroomTeacherView.tsx
@@ -26,13 +26,18 @@ import { DeleteDialog } from '../components/Dialog';
 import ClassroomLeaderboardsDialog from '../components/Dialog/ClassroomLeaderboardsDialog';
 import Challenge from '../components/Challenge';
 import { AsyncChallenge } from '../state/State/Challenge';
-import { Challenges, ChallengeCompletions } from '../state/State';
+import { Challenges, ChallengeCompletions, Classrooms } from '../state/State';
 import { Project } from 'state/State/Project';
 import TourTarget from '../components/Tours/TourTarget';
 import { TourRegistry } from '../tours/TourRegistry';
 import GuidedTour from '../components/Tours/GuidedTour';
 import TourDoc, { getTeacherViewTourSteps, getTourSteps, TourStep } from '../tours/Tours';
 import { completeTour, fetchTourIfNeeded, retakeTour } from '../state/reducer/tours';
+import TeacherTabs from '../components/Classrooms/TeacherTabs';
+import { Card } from '../components/interface/Card';
+import CreateAssignmentView from '../components/Classrooms/CreateAssignmentView';
+import AssignToDialog from '../components/Dialog/AssignToDialog';
+
 
 export interface ClassroomTeacherViewRootRouteParams {
   classroomId: string;
@@ -110,6 +115,8 @@ interface ClassroomTeacherViewState {
   deleteObject?: IvyGateClassroom | User | null;
   currentTourStepIndex?: number;
   continueTour?: boolean;
+  currentSelectedClassroom?: AsyncClassroom | null;
+  createAssignmentVisible?: boolean;
 }
 
 interface ClickProps {
@@ -142,15 +149,62 @@ const ClassroomsTitleContainer = styled('div', (props: ThemeProps) => ({
   display: 'flex',
   flexDirection: 'column',
   margin: '20px',
+
 }));
+
+const ClassroomsCardContainer = styled('div', (props: ThemeProps) => ({
+  alignItems: 'center',
+  justifyContent: 'flex-start',
+  display: 'flex',
+  flexDirection: 'row',
+  margin: '20px',
+}));
+
+const ClassroomCardScrollContainer = styled('div', {
+  width: '100%',
+  overflow: 'auto',
+  WebkitOverflowScrolling: 'touch',
+  height: '32%',
+  scrollbarWidth: 'thin',
+  scrollbarColor: 'rgba(121,121,121,0.6) transparent',
+  //backgroundColor: 'purple',
+  '::-webkit-scrollbar': {
+    width: '14px',
+    height: '14px',
+  },
+  '::-webkit-scrollbar-track': {
+    background: 'transparent',
+  },
+  '::-webkit-scrollbar-thumb': {
+    backgroundColor: 'rgba(121,121,121,0.4)',
+    borderRadius: '8px',
+  },
+  '::-webkit-scrollbar-thumb:hover': {
+    backgroundColor: 'rgba(121,121,121,0.7)',
+  },
+});
+
+const CardWrapper = styled('div', (props: ThemeProps & { selected?: boolean }) => ({
+  borderRadius: `${props.theme.itemPadding * 4}px`,
+  cursor: 'pointer',
+  backgroundColor: props.selected ? 'pink' : 'transparent',
+}));
+
+const StickyButtonWrap = styled('div', {
+  position: 'absolute',
+  left: '20px',
+  top: '6%',
+  zIndex: 2,
+});
 
 const ClassroomHeaderContainer = styled('div', (props: ThemeProps) => ({
   display: 'flex',
   flexDirection: 'row',
   justifyContent: 'center',
   gap: '3em',
-  width: '90vw'
-
+  backgroundColor: 'pink',
+  width: '90vw',
+  height: '90vh'
 }));
 
 const ManageClassroomsContainer = styled('div', (props: ThemeProps) => ({
@@ -163,7 +217,7 @@ const ManageClassroomsContainer = styled('div', (props: ThemeProps) => ({
   height: '100%',
 }));
 
-const Button = styled('div', (props: ThemeProps & ClickProps) => ({
+const Button = styled('div', (props: ThemeProps) => ({
   display: 'flex',
   alignItems: 'center',
   flexDirection: 'row',
@@ -173,7 +227,7 @@ const Button = styled('div', (props: ThemeProps & ClickProps) => ({
   ':last-child': {
     borderBottom: 'none'
   },
-  opacity: props.disabled ? '0.5' : '1.0',
+  //opacity: props.disabled ? '0.5' : '1.0',
   fontWeight: 400,
   ':hover': {
     cursor: 'pointer',
@@ -182,6 +236,9 @@ const Button = styled('div', (props: ThemeProps & ClickProps) => ({
   userSelect: 'none',
   transition: 'background-color 0.2s, opacity 0.2s'
 }));
+
+
+
 
 export const IVYGATE_LANGUAGE_MAPPING: Dict<string> = {
   'ecmascript': 'javascript',
@@ -208,6 +265,8 @@ class ClassroomTeacherView extends React.Component<Props, State> {
       challenges: {},
       showCreateClassroomDialog: false,
       isStudentInClassroom: null as boolean | null,
+      currentSelectedClassroom: null,
+
       showJoinClassroomDialog: false,
       showClassroomLeaderboardSelector: false,
       showSelectedClassroomLeaderboard: false,
@@ -229,6 +288,7 @@ class ClassroomTeacherView extends React.Component<Props, State> {
   }
 
   componentDidUpdate(prevProps: Props, prevState: State) {
+    console.log("componentDidUpdate called with props: ", this.props, " and state: ", this.state);
     if (prevProps.classroomList !== this.props.classroomList) {
       this.getIvygateClassrooms();
     }
@@ -534,41 +594,104 @@ class ClassroomTeacherView extends React.Component<Props, State> {
     void retakeTour(this.props.tour, this.props.uid, TourDoc.IDS.TEACHER_VIEW);
   };
 
+  private exisitingClassroomCards = () => {
+    const { classroomList, theme, locale } = this.props;
+
+    console.log("existingClassroomCards: ", classroomList);
+    console.log("Object.entries(classroomList): ", Object.entries(classroomList));
+    return Object.entries(classroomList).map(([id, asyncClassroom]) => {
+      console.log("Checking classroom: ", id, asyncClassroom);
+      if (asyncClassroom.type === Async.Type.Loaded && classroomList !== null) {
+        const classroom = asyncClassroom.value;
+        return (
+          <CardWrapper theme={theme} key={`${id}-wrapper`} selected={Async.latestValue(this.state.currentSelectedClassroom)?.classroomId === classroom.classroomId} onClick={() => this.setState({ currentSelectedClassroom: asyncClassroom })}>
+            <Card
+              key={id}
+              onClick={() => this.setState({ currentSelectedClassroom: asyncClassroom })}
+              title={classroom.classroomId}
+              theme={theme}
+              customheight='150px'
+              customwidth='200px'
+              backgroundImage={'linear-gradient(#3b3c3c, transparent), url(../../static/example_images/classroom-botguy.png)'}
+              backgroundPosition={'center top'}
+              custommargin='10px'
+            />
+          </CardWrapper>
+        );
+      }
+      return null;
+    }).filter(card => card !== null);
+
+  };
+
+  private handleAssignemntAction = (currentSelectedClassroom: AsyncClassroom | null, action: 'edit' | 'delete') => {
+    console.log("handleAssignmentAction called with classroom: ", currentSelectedClassroom, " and action: ", action);
+    this.setState({ createAssignmentVisible: true });
+  };
   render() {
     const { props, state } = this;
-    const { style } = props;
-    const { showAreYouSureDialog, deleteObject } = state;
+    const { style, locale } = props;
+    const { showAreYouSureDialog, deleteObject, showCreateClassroomDialog, createAssignmentVisible } = state;
     const theme = DARK;
     const showTour = props.tourLoaded && !props.tour.completed;
     return (
       <PageContainer style={style} theme={theme}>
         <MainMenu theme={theme} tourRegistry={this.registry} onRetakeTour={this.onRetakeTour_} />
         <TourTarget registry={this.registry} targetKey='teacher-dashboard' style={style}>
-          <ClassroomsContainer style={style} theme={theme}>
-            <ClassroomsTitleContainer style={style} theme={theme}>
-              <h1>{LocalizedString.lookup(tr('Classrooms - Teacher View'), props.locale)}</h1>
-              {this.props.classroomList && Object.keys(this.props.classroomList).length > 0 && (
-                <Button theme={theme} onClick={this.onSeeLeaderboards}>
-                  {LocalizedString.lookup(tr('See Classroom Leaderboards'), props.locale)}
-                </Button>)}
-              <ClassroomHeaderContainer style={style} theme={theme}>
-                {this.renderManageClassrooms()}
-                {showAreYouSureDialog && (
-                  <DeleteDialog
-                    name={tr(deleteObject && deleteObject.type === "classroom" ? `${deleteObject.name}` : deleteObject && deleteObject.type === "user" ? `${deleteObject.displayName}` : '')}
-                    onClose={this.onExitDeleteDialog_}
-                    onAccept={this.onCloseDeleteDialog_}
-                    theme={theme}>
+          {createAssignmentVisible ? (
+            <ClassroomsContainer style={style} theme={theme}>
+              <CreateAssignmentView
+                onClose={() => this.setState({ createAssignmentVisible: false })}
+                theme={theme}
+                classroom={state.currentSelectedClassroom}
 
-                  </DeleteDialog>)}
+              />
+            </ClassroomsContainer>) : (
+            <ClassroomsContainer style={style} theme={theme}>
+              <StickyButtonWrap>
+                <Button
+                  theme={theme}
+                  onClick={() => this.setState({ showCreateClassroomDialog: true })}
+                >
+                  Add New Classroom
+                </Button>
+              </StickyButtonWrap>
+              <ClassroomCardScrollContainer>
 
-              </ClassroomHeaderContainer>
+                <ClassroomsCardContainer style={style} theme={theme}>
 
-            </ClassroomsTitleContainer>
+                  {this.exisitingClassroomCards()}
 
-          </ClassroomsContainer>
+                </ClassroomsCardContainer>
+              </ClassroomCardScrollContainer>
+              <TeacherTabs theme={theme} currentSelectedClassroom={this.state.currentSelectedClassroom} onAssignmentAction={this.handleAssignemntAction} />
+
+
+              {showAreYouSureDialog && (
+                <DeleteDialog
+                  name={tr(deleteObject && deleteObject.type === "classroom" ? `${deleteObject.name}` : deleteObject && deleteObject.type === "user" ? `${deleteObject.displayName}` : '')}
+                  onClose={this.onExitDeleteDialog_}
+                  onAccept={this.onCloseDeleteDialog_}
+                  theme={theme}>
+
+                </DeleteDialog>)}
+              {
+                showCreateClassroomDialog && (
+                  <CreateClassroomDialog
+                    onClose={this.onExitCreateClassroomDialog_}
+                    onContinueTour={this.onContinueTour_}
+                    onCloseClassroomDialog={this.onCloseClassroomDialog_}
+                    theme={DARK}
+                    locale={locale}
+                    tourRegistry={this.registry}
+                  />
+                )}
+
+            </ClassroomsContainer>)
+          }
 
         </TourTarget>
+
         {showTour && (
           <GuidedTour
             continueTourFlag={this.state.continueTour}
