de2c57260d07b46559f467a10880f6d7a1509e92
Erin <erinharrington12@yahoo.com>
2026-08-25 15:06:32 -0500

    Added subfolders to open scenes

--- src/components/constants/defaultScene.ts
+++ src/components/constants/defaultScene.ts
@@ -4,3 +4,20 @@ export const DEFAULT_SCENE = `/scene/${JBC_SANDBOX_SCENE_ID}`;
 
 /** Sentinel id for the Open World dialog's "Create Your Own Challenge" row (loads JBC Sandbox). */
 export const CREATE_YOUR_OWN_SCENE_OPTION_ID = '__create_your_own_scene__';
+
+export const BEX_SANDBOX_SCENE_ID = 'botballExplorer26Sandbox';
+
+export const ARCHIVED_SCENES =
+  [
+    'spring25TableSandbox',
+    'Find_The_Black_Line',
+    'Sense_The_Can',
+    'Ice_Ice_Botguy',
+    'Thirst_Quencher',
+    'Entree_Express',
+    'Special_Sauce',
+    'Cover_Your_Bases',
+    'Odd_Numbers',
+    'Bulldozer_Mania',
+    'Mountain_Rescue',
+  ];
