2fdc772732d5a2b1480255d1d7eb47c743be73cc
Nathan Ngo <ndatngo.cs@gmail.com>
2026-09-11 17:11:47 +0000

    Update Root.tsx to use theme setting

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -17,7 +17,7 @@ import { SimulatorState } from '../components/Challenge/SimulatorState';
 import ExceptionDialog from '../components/Challenge/ExceptionDialog';
 
 import { DEFAULT_SETTINGS, Settings } from '../components/constants/Settings';
-import { DARK, Theme } from '../components/constants/theme';
+import { DARK, LIGHT, Theme } from '../components/constants/theme';
 import { DEFAULT_SCENE } from '../components/constants/defaultScene';
 import CustomChallengeSetupDialog from '../components/Dialog/CustomChallengeSetupDialog';
 
@@ -1417,7 +1417,7 @@ class Root extends React.Component<Props, State> {
       simulatorRootTourSteps,
     } = state;
 
-    const theme = DARK;
+    const theme = settings.ideEditorDarkMode ? DARK : LIGHT;
 
     const editorTarget: LayoutEditorTarget = {
       type: LayoutEditorTarget.Type.Robot,
@@ -1767,7 +1767,7 @@ class Root extends React.Component<Props, State> {
           />
         )}
         <AiWindow
-          theme={DARK}
+          theme={theme}
           code={code[activeLanguage]}
           language={activeLanguage}
           console={StyledText.toString(console)}
