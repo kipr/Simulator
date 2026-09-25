c07bab3406f095f6bad6e5a50ea10c00e52cdee9
Nathan Ngo <ndatngo.cs@gmail.com>
2026-09-25 16:10:35 +0000

    Update Card to use white text

--- src/components/interface/Card.tsx
+++ src/components/interface/Card.tsx
@@ -82,7 +82,7 @@ const Gradient = styled('div', (props: ThemeProps) => ({
 const Header = styled('div', (props: ThemeProps) => ({
   fontSize: '2em',
   position: 'absolute',
-  color: props.theme.textColor,
+  color: props.theme.cardColors.alternateTextColor,
   fontWeight: 800,
   alignSelf: 'flex-start',
   marginLeft: `${props.theme.itemPadding * 2}px`,
@@ -93,7 +93,7 @@ const Description = styled('div', (props: ThemeProps) => ({
   fontSize: '1em',
   position: 'absolute',
   top: '3em',
-  color: props.theme.textColor,
+  color: props.theme.cardColors.alternateTextColor,
   fontWeight: 400,
   alignSelf: 'flex-start',
   marginLeft: `${props.theme.itemPadding * 2}px`,
